5c2f528b73f3f688e6645df26e8e354d8510b04c
Wesley Ketchum <wketchum@fnal.gov>
2014-02-18 11:59:35 -0600

    additions for ttspacepointfinder to compile and run

--- SpacePointFinder/TTSpacePointFinder_module.cc
+++ SpacePointFinder/TTSpacePointFinder_module.cc
@@ -29,6 +29,10 @@
 #include "Utilities/DetectorProperties.h"
 #include "RecoBase/Hit.h"
 #include "RecoBase/SpacePoint.h"
+#include "Utilities/AssociationUtil.h"
+
+//boost includes
+#include "boost/multi_array.hpp"
 
 bool  HitTimeComparison(art::Ptr<recob::Hit> a, art::Ptr<recob::Hit> b) { return a->PeakTime() < b->PeakTime(); }
 
@@ -42,7 +46,8 @@ namespace sppt{
     virtual ~TTSpacePointFinder();
          
     void produce(art::Event& evt); 
-    void beginJob(); 
+    void beginJob();
+    void beginRun(art::Run& run);
     void endJob(); 
     void reconfigure(fhicl::ParameterSet const& p);                
 
@@ -63,8 +68,10 @@ namespace sppt{
     double TIME_OFFSET_Y;
     double TICKS_TO_X;
 
-    std::vector< std::vector< std::pair<double,double> > > coordinates_UV;
-    std::vector< std::vector< std::pair<double,double> > > coordinates_UY;
+    boost::multi_array<double, 2> coordinates_UV_y;//(boost::extents[1][1]);
+    boost::multi_array<double, 2> coordinates_UV_z;//(boost::extents[1][1]);
+    boost::multi_array<double, 2> coordinates_UY_y;//(boost::extents[1][1]);
+    boost::multi_array<double, 2> coordinates_UY_z;//(boost::extents[1][1]);
 
   protected:     
   
@@ -107,43 +114,43 @@ namespace sppt{
   }
 
   //-------------------------------------------------
-  void TTSpacePointFinder::beginJob(){
-    
-    art::ServiceHandle<util::DetectorProperties> detprop;
+  void TTSpacePointFinder::beginJob(){}
+
+  //-------------------------------------------------
+  void TTSpacePointFinder::beginRun(art::Run &run){
+        art::ServiceHandle<util::DetectorProperties> detprop;
 
     TIME_OFFSET_U = -1*detprop->GetXTicksOffset(geo::View_t::kU,0,0);
     TIME_OFFSET_V = -1*detprop->GetXTicksOffset(geo::View_t::kV,0,0);
     TIME_OFFSET_Y = -1*detprop->GetXTicksOffset(geo::View_t::kZ,0,0);
 
     TICKS_TO_X = detprop->GetXTicksCoefficient();
-
+    
     art::ServiceHandle<geo::Geometry> geom;
     unsigned int nwires_u = geom->Nwires(geo::View_t::kU);
     unsigned int nwires_v = geom->Nwires(geo::View_t::kV);
     unsigned int nwires_y = geom->Nwires(geo::View_t::kZ);
-
-    coordinates_UV.resize(nwires_v);
-    for(unsigned int iv=0; iv<nwires_v; iv++){
-      (coordinates_UV.at(iv)).resize(nwires_u);
-      for(unsigned int iu=0; iu<nwires_u; iu++){
+    
+    coordinates_UV_y.resize(boost::extents[nwires_v][nwires_u]);
+    coordinates_UV_z.resize(boost::extents[nwires_v][nwires_u]);
+    coordinates_UY_y.resize(boost::extents[nwires_y][nwires_u]);
+    coordinates_UY_z.resize(boost::extents[nwires_y][nwires_u]);
+    for(unsigned int iu=0; iu<nwires_u; iu++){
+      for(unsigned int iv=0; iv<nwires_v; iv++){
 	geom->IntersectionPoint(iu,iv,
 				geo::View_t::kU,geo::View_t::kV,
 				0,0,
-				coordinates_UV[iv][iu].first, //y
-				coordinates_UV[iv][iu].second); //z
+				coordinates_UV_y[iv][iu], //y
+				coordinates_UV_z[iv][iu]); //z
       }
-    }
-    coordinates_UY.resize(nwires_y);
-    for(unsigned int iy=0; iy<nwires_y; iy++){
-      (coordinates_UV.at(iy)).resize(nwires_u);
-      for(unsigned int iu=0; iu<nwires_u; iu++){
+      for(unsigned int iy=0; iy<nwires_y; iy++){
 	geom->IntersectionPoint(iu,iy,
 				geo::View_t::kU,geo::View_t::kZ,
 				0,0,
-				coordinates_UV[iy][iu].first,
-				coordinates_UV[iy][iu].second);
+				coordinates_UY_y[iy][iu],
+				coordinates_UY_z[iy][iu]);
       }
-    }
+    }	    
   }
 
   //-------------------------------------------------
@@ -155,6 +162,7 @@ namespace sppt{
 
     //initialize our spacepoint collection
     std::unique_ptr<std::vector<recob::SpacePoint> > spptCollection(new std::vector<recob::SpacePoint>);
+    std::unique_ptr<art::Assns<recob::SpacePoint,recob::Hit> > spptAssns(new art::Assns<recob::SpacePoint,recob::Hit>);
 
     // Read in the hits. Note, we will reorder hit vector, so we do in fact need a copy.
     art::Handle< std::vector<recob::Hit> > hitHandle_U;
@@ -172,11 +180,19 @@ namespace sppt{
     std::vector< art::Ptr<recob::Hit> > hitVec_Y;
     art::fill_ptr_vector(hitVec_Y,hitHandle_Y);
 
+    mf::LogInfo("TTSpacePointFinderDetail") 
+      << "Got handles to hits."; 
     
     //sort the hits by the time
     sortHitsByTime(hitVec_U);
     sortHitsByTime(hitVec_V);
     sortHitsByTime(hitVec_Y);
+
+    mf::LogInfo("TTSpacePointFinderDetail") 
+      << "Sorted " 
+      << hitVec_U.size() << " u hits, "
+      << hitVec_V.size() << " v hits, "
+      << hitVec_Y.size() << " y hits.";
 
     //now, do the loop to search for like-timed hits across the three planes
     std::vector< art::Ptr<recob::Hit> >::iterator ihitu = hitVec_U.begin();
@@ -189,6 +205,12 @@ namespace sppt{
     double time_hitv_inner,time_hity_inner;
     while(ihitu != hitVec_U.end()){
       time_hitu = (*ihitu)->PeakTime() + TIME_OFFSET_U;
+
+      mf::LogInfo("TTSpacePointFinderDetail") 
+	<< "Hit times (u,v,y)=("
+	<< time_hitu << ","
+	<< time_hitv << ","
+	<< time_hity << ")"; 
 
       //if time_hitu is too much bigger than time_hitv, need to advance hitv iterator
       while( (time_hitu-time_hitv)>fTimeDiffMax ){
@@ -218,6 +240,12 @@ namespace sppt{
       //  -- time_hitu is within fTimeDiffMax of both time_hitv and time_hity; and
       //  -- time_hitu <= time_hitv AND time_hitu <=time_hity, so time_hitv and time_hity are near too
       
+      mf::LogInfo("TTSpacePointFinderDetail") 
+	<< "Matching hit times (u,v,y)=("
+	<< time_hitu << ","
+	<< time_hitv << ","
+	<< time_hity << ")"; 
+
       //Next thing to do, we need to loop over all possible 3-hit matches for our given u-hit.
       //We need new iterators in v and y at this location, and will loop over those
       ihitv_inner = ihitv;
@@ -225,14 +253,21 @@ namespace sppt{
       ihity_inner = ihity;
       time_hity_inner = (*ihity_inner)->PeakTime() + TIME_OFFSET_Y;
 
-      while(std::abs(time_hitu-time_hitv_inner)>fTimeDiffMax || std::abs(time_hitu-time_hity_inner)>fTimeDiffMax){
+      while(std::abs(time_hitu-time_hitv_inner)<fTimeDiffMax && std::abs(time_hitu-time_hity_inner)<fTimeDiffMax){
 
 	unsigned int uwire = (*ihitu)->WireID().Wire;
 	unsigned int vwire = (*ihitv_inner)->WireID().Wire;
 	unsigned int ywire = (*ihity_inner)->WireID().Wire;
 
-	if(std::abs(coordinates_UV[vwire][uwire].first-coordinates_UY[vwire][uwire].first)<fYDiffMax &&
-	   std::abs(coordinates_UV[vwire][uwire].second-coordinates_UY[vwire][uwire].second)<fZDiffMax){
+	mf::LogInfo("TTSpacePointFinderDetail") 
+	  << "(y,z) coordinate for uv/uy: ("
+	  << coordinates_UV_y[vwire][uwire] << ","
+	  << coordinates_UV_z[vwire][uwire] << ")/("
+	  << coordinates_UY_y[ywire][uwire] << ","
+	  << coordinates_UY_z[ywire][uwire] << ")";
+
+	if(std::abs(coordinates_UV_y[vwire][uwire]-coordinates_UY_y[ywire][uwire])<fYDiffMax &&
+	   std::abs(coordinates_UV_z[vwire][uwire]-coordinates_UY_z[ywire][uwire])<fZDiffMax){
 
 	  double xyz[3];
 	  double xyz_err[6];
@@ -242,10 +277,10 @@ namespace sppt{
 	  // | 0.  0.  0. |
 
 	  //get average y and z, with errors
-	  xyz[1] = (coordinates_UV[vwire][uwire].first + coordinates_UY[ywire][uwire].first)*0.5;
-	  xyz_err[2] = std::abs(coordinates_UV[vwire][uwire].first - xyz[1]);
-	  xyz[2] = (coordinates_UV[vwire][uwire].second + coordinates_UY[ywire][uwire].second)*0.5;
-	  xyz_err[5] = std::abs(coordinates_UV[vwire][uwire].second - xyz[2]);
+	  xyz[1] = (coordinates_UV_y[vwire][uwire] + coordinates_UY_y[ywire][uwire])*0.5;
+	  xyz_err[2] = std::abs(coordinates_UV_y[vwire][uwire] - xyz[1]);
+	  xyz[2] = (coordinates_UV_z[vwire][uwire] + coordinates_UY_z[ywire][uwire])*0.5;
+	  xyz_err[5] = std::abs(coordinates_UV_z[vwire][uwire] - xyz[2]);
 
 	  double t_val = (time_hitu + time_hitv_inner + time_hity_inner)/3.;
 	  double t_err = 0.5*std::sqrt( (time_hitu-t_val)*(time_hitu-t_val) +
@@ -254,8 +289,13 @@ namespace sppt{
 	  xyz[0] = TICKS_TO_X * t_val;
 	  xyz_err[0] = TICKS_TO_X * t_err;
 	  
+	  //make space point to put on event
 	  recob::SpacePoint spt(xyz, xyz_err, 0., spptCollection->size());
 	  spptCollection->push_back(spt);
+
+	  //make association with hits
+	  std::vector< art::Ptr<recob::Hit> > myhits = {*ihitu,*ihitv_inner,*ihity_inner};
+	  util::CreateAssn(*this, evt, *spptCollection, myhits, *spptAssns);
 	}
 
 	//now increment the v or y hit, whichever is smalles (closest to u hit) in time
@@ -275,6 +315,10 @@ namespace sppt{
       ihitu++;
     }
     
+    //finally, put things on the event
+    evt.put(std::move(spptCollection));
+    evt.put(std::move(spptAssns));
+
   } // End of produce()  
     
   //-------------------------------------------------
