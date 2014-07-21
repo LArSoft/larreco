6921e855608ee33ba15498df436e5a13e4d0a3ed
Ben Carls <bcarls@localhost.localdomain>
2014-07-21 09:50:09 -0500

    Updating cluster crawler shower stuff

--- ClusterFinder/ClusterCrawlerShower_module.cc
+++ ClusterFinder/ClusterCrawlerShower_module.cc
@@ -1,219 +1,267 @@
 ////////////////////////////////////////////////////////////////////////
+// Class:       ClusterCrawlerShower
+// Module Type: producer
+// File:        ClusterCrawlerShower_module.cc
 //
-//  
+// Generated at Tue May 27 14:15:41 2014 by Kazuhiro Terao using artmod
+// from cetpkgsupport v1_05_04.
 ////////////////////////////////////////////////////////////////////////
 
-#include <string>
-#include <math.h>
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <vector>
-
-//Framework includes:
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Core/ModuleMacros.h" 
+#include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
-#include "fhiclcpp/ParameterSet.h"
 #include "art/Framework/Principal/Handle.h"
-#include "art/Persistency/Common/Ptr.h"
-#include "art/Persistency/Common/PtrVector.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "art/Framework/Services/Optional/TFileService.h"
-#include "art/Framework/Services/Optional/TFileDirectory.h"
-#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
 
-//LArSoft includes:
 #include "Geometry/Geometry.h"
-#include "RecoBase/Cluster.h"
-#include "RecoBase/Hit.h"
 #include "Utilities/AssociationUtil.h"
-//#include "RecoAlg/ClusterParamsAlg.h"
 #include "RecoAlg/ClusterMergeHelper.h"
+#include "RecoAlg/CMTool/CMAlgoMergeAll.h"
+#include "RecoAlg/CMTool/CMAlgoArray.h"
+#include "RecoAlg/CMTool/CMAlgoShortestDist.h"
+#include "RecoAlg/CMTool/CMAlgoShortestDistSmallCluster.h"
+#include "RecoAlg/CMTool/CMAlgoShortestDistNonEndpoint.h"
+#include "RecoAlg/CMTool/CMAlgoAngleCompat.h"
+#include "RecoAlg/CMTool/CMAlgoTrackSeparate.h"
 
-
-
-#ifndef CLUSTERCRAWLERSHOWER_H
-#define CLUSTERCRAWLERSHOWER_H
-
+#include <memory>
 
 namespace cluster {
-   
+
   class ClusterCrawlerShower : public art::EDProducer {
     
   public:
+
+    explicit ClusterCrawlerShower(fhicl::ParameterSet const & p);
+
+    virtual ~ClusterCrawlerShower();
     
-    explicit ClusterCrawlerShower(fhicl::ParameterSet const& pset); 
-    ~ClusterCrawlerShower();
-    
-    void produce(art::Event& evt);
-    void beginJob();
-    void reconfigure(fhicl::ParameterSet const& p);
+    void produce(art::Event & evt) override;
     
   private:
-        
 
-    unsigned int  fMinHitListSize;	 
-    double	  fMaxDistance;	 
+    /// ClusterMergeHelper
+    ClusterMergeHelper fCMerge;
+
+    /// Input cluster data product producer name label
+    std::string fClusterModuleLabel;
+
+    bool fDistAlg0Verbose;        // 
+    int fDistAlg0MinHits;           // 
+    int fDistAlg0MaxHits;           // 
+    double fDistAlg0SquaredDistanceCut; // 
+
+    bool fDistAlg1Verbose;        // 
+    int fDistAlg1MinHits;           // 
+    double fDistAlg1SquaredDistanceCut; // 
+
+    /// GeometryUtilities instance
+    ::util::GeometryUtilities fGeoU;
+
+    //--- CMAlgo instances ---//
+    /*
+      CMergeManager takes pointer to CBoolAlgoBase inherit class instance.
+      So the simplest way is to create them on heap and keep the pointers.
+      But that has a concern to computation speed (though I think that would
+      never be a slow enough to be a concern)... so here we take an example
+      of using CMAlgo instances created on stack.
+    */
 
 
-    //ClusterParamsAlg fCParAlg;
-    //ClusterMergeAlg fCMergeAlg;
-   
-    std::string     fClusterCrawlerModuleLabel;
+
+
+    /// Algo 0: Merge clusters that are small in order to do shower recognition
+    CMAlgoArray fMergeAlg0;
+    /// Algo 0: Distance algorithm for small cluster merging
+    CMAlgoShortestDistSmallCluster fDistAlg0;
+
+    /// Algo 1: Merge shower-like clusters that intercept
+    CMAlgoArray fMergeAlg1;
+    /// Algo 0: Distance algorithm for shower-like clusters that intercept, it should be 0
+    CMAlgoShortestDist	fDistAlg1;
+
+
+
+
+
   
-  
-  protected: 
-    
-  }; // class ClusterCrawlerShower
-
+  };
 }
 
-#endif // CLUSTERCRAWLERSHOWER_H
+namespace cluster {
 
-
-
-//namespace cluster{
-
-  //-------------------------------------------------
-cluster::ClusterCrawlerShower::ClusterCrawlerShower(fhicl::ParameterSet const& pset) 
-//: fCParAlg(pset.get<fhicl::ParameterSet >("ClusterParamsAlg"),pset.get< std::string > ("module_type"))
-//: fCMergeAlg(pset.get<fhicl::ParameterSet >("ClusterMergeAlg"))
-  : fClusterCrawlerModuleLabel(pset.get<std::string>("ClusterCrawlerModuleLabel"))
-{
-  this->reconfigure(pset);
-  produces< std::vector<recob::Cluster> >();
-  produces< art::Assns<recob::Cluster, recob::Hit> >();
-}
-
-
-  void cluster::ClusterCrawlerShower::reconfigure(fhicl::ParameterSet const& pset) 
+  ClusterCrawlerShower::ClusterCrawlerShower(fhicl::ParameterSet const & p)
   {
-    fClusterCrawlerModuleLabel	=pset.get< std::string >("ClusterCrawlerModuleLabel");
-    //fCParAlg.reconfigure(pset.get< fhicl::ParameterSet >("ClusterParamsAlg"));
-    fMinHitListSize	  	=pset.get<unsigned int >("MinHitListSize");
-    fMaxDistance		=pset.get<double>("MaxDistance");
-  }
-
-  //-------------------------------------------------
-  cluster::ClusterCrawlerShower::~ClusterCrawlerShower()
-  {
-  }
-
-  //-------------------------------------------------
-  void cluster::ClusterCrawlerShower::beginJob()
-  {
-    //this doesn't do anything now, but it might someday
-  }
+    // Declare output data products
+    produces< std::vector<recob::Cluster> >();
+    produces< art::Assns<recob::Cluster, recob::Hit> >();
     
-  //------------------------------------------------------------------------------------//
-  void cluster::ClusterCrawlerShower::produce(art::Event& evt)
-  { 
-    // Get a Handle for the input Cluster object(s).
-    art::Handle< std::vector<recob::Cluster> > clusterVecHandle;
-    evt.getByLabel(fClusterCrawlerModuleLabel,clusterVecHandle);
+    // Fill fcl parameter
+    fClusterModuleLabel = p.get<std::string>("InputClusterLabel");
+    
+    fDistAlg0Verbose = p.get<bool>("DistAlg0Verbose");
+    fDistAlg0MinHits = p.get<int>("DistAlg0MinHits");
+    fDistAlg0MaxHits = p.get<int>("DistAlg0MaxHits");
+    fDistAlg0SquaredDistanceCut = p.get<double>("DistAlg0SquaredDistanceCut");
+    
+    fDistAlg1Verbose = p.get<bool>("DistAlg1Verbose");
+    fDistAlg1MinHits = p.get<int>("DistAlg1MinHits");
+    fDistAlg1SquaredDistanceCut = p.get<double>("DistAlg1SquaredDistanceCut");
 
-    // Get a Handle for the input hits, assuming for the time being that the hits label will be that of Cluster Crawler
-    art::Handle< std::vector<recob::Hit> > hitVecHandle;
-    evt.getByLabel(fClusterCrawlerModuleLabel,hitVecHandle);
+    //--- Configure Merging Algorithm ---//
+    /*
+      This is where we should pass fcl parameters to configure various algorithms
+      we defined as class member. Here, for simplicity, I don't pass any configuration
+      parameters but you should in your custom merging module.
+    */
 
-    art::ServiceHandle<geo::Geometry> geom;
-    art::ServiceHandle<util::LArProperties> larprop;
-    art::ServiceHandle<util::DetectorProperties> detprop;
 
-    art::FindManyP<recob::Hit> fmh(clusterVecHandle, evt, fClusterCrawlerModuleLabel);
+
+    /// Configure algorithm 0, Merges small clusters in order to perform shower recognition
+    fDistAlg0.SetVerbose(fDistAlg0Verbose);        // No verbous mode ... annoying
+    fDistAlg0.SetMinHits(fDistAlg0MinHits);           // Set minimum # hits 
+    fDistAlg0.SetMaxHits(fDistAlg0MaxHits);           // Set minimum # hits 
+    fDistAlg0.SetSquaredDistanceCut(fDistAlg0SquaredDistanceCut); // Set distance-squared cut, is in cm^2 
+    fMergeAlg0.AddAlgo(&fDistAlg0,true); // attach to CMAlgoArray in AND condition
+
+    /// Configure algorithm 0, Merge shower like clusters that intercept
+    fDistAlg1.SetVerbose(fDistAlg1Verbose);        // No verbous mode ... annoying
+    fDistAlg1.SetMinHits(fDistAlg1MinHits);           // Set minimum # hits 
+    fDistAlg1.SetSquaredDistanceCut(fDistAlg1SquaredDistanceCut); // Set distance-squared cut, it should be 0 and is in cm^2 
+    fMergeAlg1.AddAlgo(&fDistAlg1,true); // attach to CMAlgoArray in AND condition
+
+
+
+
+
+    //--- Configure Merger ---//
+    fCMerge.GetManager().AddMergeAlgo(&fMergeAlg0);        // Attach merging  algorithm
+    //fCMerge.GetManager().AddMergeAlgo(&fMergeAlg1);        // Attach merging  algorithm
+
+
+
+
+
+    fCMerge.GetManager().SetMergePriority(::cluster::CMergeManager::kIndex); // Choose input cluster vector ordering as merging priority order
+    fCMerge.GetManager().DebugMode(::cluster::CMergeManager::kPerIteration); // Set verbosity level to be per-merging-iteration report
+    //fCMerge.GetManager().DebugMode(::cluster::CMergeManager::kPerMerging); // Set verbosity level to be per-merging-iteration report
+    fCMerge.GetManager().MergeTillConverge(true);         // Set to iterate over till it finds no more newly merged clusters
+
+    //
+    // FYI there's an algorithm to just-merge-everything if you want to do a simple test (line commented out below)
+    //
+    //fCMerge.GetManager().AddMergeAlgo( new CMAlgoMergeAll );
 
     
-    // make a map of the geo::View to vectors of art::Ptr<recob::Hit>
-    std::map<geo::View_t, std::vector< art::Ptr<recob::Hit> > > viewToHits;
-    for(size_t i = 0; i < hitVecHandle->size(); ++i)
-      viewToHits[hitVecHandle->at(i).View()].push_back(art::Ptr<recob::Hit>(hitVecHandle, i));
-
-
-    //fCMergeAlg.ClearEventInfo();
-    std::vector<recob::Cluster> inputShowerClusters;
-    std::vector<std::vector<art::Ptr<recob::Hit> > > inputShowerClusterHits;
-    for(unsigned int iClust = 0; iClust < clusterVecHandle->size(); iClust++){
-
-      art::Ptr<recob::Cluster> cl(clusterVecHandle, iClust);
-      // Hits that are in the cluster
-      std::vector< art::Ptr<recob::Hit> > hitlist = fmh.at(iClust);
-      std::vector< art::Ptr<recob::Hit> > showerHitList;
-
-
-      double wirePitch = geom->WirePitch(cl->View());
-      double xyScale  = .001*larprop->DriftVelocity(larprop->Efield(),larprop->Temperature());
-      xyScale        *= detprop->SamplingRate()/wirePitch;
-
-      if(hitlist.size()<=fMinHitListSize)
-        continue;
-
-
-      // Need to find the hits in and around the cluster crawler cluster
-      //for(size_t i = 0; i < viewToHits[cl->View()].size(); i++){
-      for(auto hitsItr = viewToHits[cl->View()].begin(); hitsItr < viewToHits[cl->View()].end(); hitsItr++){
-
-	//std::cout << (*hitsItr)->StartTime() << std::endl;
-
-	double clIntercept = cl->StartPos().back() - cl->dTdW()*cl->StartPos().front();
-	double distance = (TMath::Abs((*hitsItr)->PeakTime()-cl->dTdW()*(double)((*hitsItr)->WireID().Wire)-clIntercept)/(std::sqrt(pow(xyScale*cl->dTdW(),2)+1)));
-
-	// Sum up background hits, use smart distance
-	double peakTimePerpMin=-(1/cl->dTdW())*(double)((*hitsItr)->WireID().Wire)+cl->StartPos().back()+(1/cl->dTdW())*(cl->StartPos().front());
-	double peakTimePerpMax=-(1/cl->dTdW())*(double)((*hitsItr)->WireID().Wire)+cl->EndPos().back()+(1/cl->dTdW())*(cl->EndPos().front());
-
-
-	if(distance > 1*(fMaxDistance+(((*hitsItr)->EndTime()-(*hitsItr)->StartTime())/2.))
-         && distance < 25*(fMaxDistance+(((*hitsItr)->EndTime()-(*hitsItr)->StartTime())/2.))){
-	  if((cl->dTdW() < 0 && (*hitsItr)->PeakTime() < peakTimePerpMin && (*hitsItr)->PeakTime() > peakTimePerpMax)
-            || (cl->dTdW() > 0 && (*hitsItr)->PeakTime() > peakTimePerpMin && (*hitsItr)->PeakTime() < peakTimePerpMax)){
-	  showerHitList.push_back(*hitsItr);
-	  }
-	}
-      }
-
-      /*
-      double wire_start = cl->StartPos().front();
-      double wire_end = cl->EndPos().front();
-      double time_start = cl->StartPos().back();
-      double time_end = cl->EndPos().back();
-
-      if(!fCParAlg.isShower(cl->dTdW(),
-			    wire_start,
-			    time_start,
-			    wire_end,
-			    time_end,
-			    showerHitList))
-	continue;
-      */
+  }
+  
+  ClusterCrawlerShower::~ClusterCrawlerShower()
+  {
+    // Clean up dynamic memory and other resources here.
+  }
+  
+  void ClusterCrawlerShower::produce(art::Event & evt)
+  {
+    std::unique_ptr<std::vector<recob::Cluster> > out_clusters(new std::vector<recob::Cluster>);
+    std::unique_ptr<art::Assns<recob::Cluster, recob::Hit> > out_assn(new art::Assns<recob::Cluster, recob::Hit>);
+    
+    art::ServiceHandle<geo::Geometry> geo;
+    
+    // Implementation of required member function here.
+    
+    //--- Set input cluster data ---//
+    fCMerge.SetClusters(evt, fClusterModuleLabel);
+    
+    //--- Process merging ---//
+    fCMerge.Process();
+    
+    //--- Retrieving Output ---//
+    /*
+      There are 2 kinds of output (that I can think of) you may want to retrieve:
       
-      //// Store this processed shower-like cluster
-      //inputShowerClusters.push_back(std::move(temp));
-      //inputShowerClusterHits.push_back(hitlist);
+      (0) A vector of merged cluster set (i.e. output) in terms of a collection of input cluster vector index
+          Most basic information (which may be too basic to be useful for most users).
+          You can retrieve through CBookKeeper instance:
+      
+	  fCMerge.GetManager().GetBookKeeper().GetResult();
 
-      //// Feed this cluster & hitlist into Merge algorithm
-      //fCMergeAlg.AppendClusterInfo(inputShowerClusters.back(),hitlist);
+      (1) A vector of CPAN
+          This is practically the most useful thing. CPAN = a set of reconstructed cluster parameters.
+	  You get a vector of CPAN corresponding a vector of merged clusters.
+	  Note ClusterMergeHelper does not create recob::Cluster by itself after running Process() function.
+	  To retrieve, try:
 
-    } // End loop on clusters.
+	  fCMerge.GetMergedCPAN();
 
+      (2) A vector of clusters in terms of hit list
+          which means std::vector<std::vector<art::Ptr<recob::Hit> > >, each content representing a
+	  cluster by its hit list. 
+	  To retrieve, try:
 
+	  fCMerge.GetMergedClusterHits();
 
-    //evt.put(std::move(SuperClusters));
-    //evt.put(std::move(assn));
+      (3) Fill std::vector<recob::Cluster> and art::Assns<recob::Cluster,recob::Hit> through pass-by-reference
+          This may be useful if you want to just store the output in LArSoft data product (and if the output
+	  of this merging process is the final result). This basically uses a return vector from (1) and (2)
+	  above to create output recob::Cluster and associations to a hit list. ClusterMergeHelper can help
+	  you this by a single function call.
+	  For this, try:
 
-    return;
+	  fCMerge.AppendResult( this,
+	                        evt,
+			        *(out_clusters.get()), 
+			        *(out_assn.get())
+			       );
 
+      In the following, as an example, I show how (3) can be done using (1) and (2). Hopefully this gives an
+      idea of what kind of (detail) information you can retrieve. 
+    */
+
+    // Store output
+    for(size_t out_index=0; out_index < fCMerge.GetMergedCPAN().size(); ++out_index) {
+      
+      // To save typing let's just retrieve const cluster_params instance
+      const cluster_params &res = fCMerge.GetMergedCPAN().at(out_index).GetParams();
+      
+      // View_t needed but not a part of cluster_params, so retrieve it here
+      geo::View_t view_id = geo->Plane(fCMerge.GetMergedCPAN().at(out_index).Plane()).View();
+      
+      // Push back a new cluster data product with parameters copied from cluster_params
+      out_clusters->push_back( recob::Cluster( res.start_point.w / fGeoU.WireToCm(), 0,  // start wire & error
+					       res.start_point.t / fGeoU.TimeToCm(), 0,  // start time & error
+					       res.end_point.w   / fGeoU.WireToCm(), 0,  // end   wire & error
+					       res.end_point.t   / fGeoU.TimeToCm(), 0,  // end   time & error
+					       res.cluster_angle_2d,                 0,  // dT/dW (slope)
+					       0,                                    0,  // dQ/dW (what is that?)
+					       res.sum_charge,                           // charge sum
+					       view_id,                                  // geo::View_t
+					       out_clusters->size()                      // Cluster ID
+					       )
+			       );
+      
+      util::CreateAssn(*this, 
+		       evt, 
+		       *(out_clusters.get()), 
+		       fCMerge.GetMergedClusterHits().at(out_index), 
+		       *(out_assn.get())
+		       );
+
+    }
+    
+    // As advertized, the following (commented-out) line does the same thing
+    /*
+      
+    fCMerge.AppendResult( *this, evt, *(out_clusters.get()), *(out_assn.get()) );
+    
+  */
+
+  evt.put(std::move(out_clusters));
+  evt.put(std::move(out_assn));
+    
   }
+}
 
-
-//} // end namespace 
-
-
-
-namespace cluster{
-
-  DEFINE_ART_MODULE(ClusterCrawlerShower)
-  
-} // end namespace 
-
+DEFINE_ART_MODULE(cluster::ClusterCrawlerShower)
