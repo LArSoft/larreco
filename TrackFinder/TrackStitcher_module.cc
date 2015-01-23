1b1a85d8535785408dea9a4bb2213c15569aad92
Eric Church <echurch@fnal.gov>
2015-01-22 19:42:57 -0600

    Add creation of Assns:Sppts,Hits in Stitcher. May still be buggy...

--- TrackFinder/TrackStitcher_module.cc
+++ TrackFinder/TrackStitcher_module.cc
@@ -87,6 +87,7 @@ namespace trkf {
 
     const art::PtrVector<recob::Hit> GetHitsFromComponentTracks(const art::PtrVector<recob::Track> &, const art::Event& evt);
     const art::PtrVector<recob::SpacePoint> GetSpacePointsFromComponentTracks(const art::PtrVector<recob::Track> &, const art::Event& evt);
+    const art::PtrVector<recob::Hit> GetHitsFromAssdSpacePoints(const art::PtrVector<recob::SpacePoint> &, const art::Event& evt);
     std::string     fTrackModuleLabel;// label for input collection
     std::string     fSpptModuleLabel;// label for input collection
     bool            fStizatch; // CommonComponentStitch
@@ -109,6 +110,7 @@ namespace trkf {
     produces<std::vector<art::PtrVector<recob::Track> >  >(); 
     produces<art::Assns<recob::Track, recob::Hit>        >();
     produces<art::Assns<recob::Track, recob::SpacePoint> >();
+    produces<art::Assns<recob::SpacePoint, recob::Hit>   >();
     // get the random number seed, use a random default if not specified    
     // in the configuration file.  
     unsigned int seed = pset.get< unsigned int >("Seed", sim::GetRandomNumberSeed());
@@ -156,10 +158,19 @@ namespace trkf {
     //////////////////////////////////////////////////////
     // tcol is the collection of new tracks
     std::unique_ptr<std::vector<recob::Track> > tcol(new std::vector<recob::Track>);
+    std::unique_ptr<std::vector<recob::SpacePoint> > scol(new std::vector<recob::SpacePoint>);
     // tvcol is the collection of vectors that comprise each tcol
     std::unique_ptr<std::vector< art::PtrVector<recob::Track> > > tvcol(new std::vector< art::PtrVector<recob::Track> >);
     std::unique_ptr< art::Assns<recob::Track, recob::Hit> > thassn(new art::Assns<recob::Track, recob::Hit>);     
     std::unique_ptr< art::Assns<recob::Track, recob::SpacePoint> > tsptassn(new art::Assns<recob::Track, recob::SpacePoint>);     
+    std::unique_ptr< art::Assns<recob::SpacePoint, recob::Hit > > spthassn(new art::Assns<recob::SpacePoint, recob::Hit>);     
+
+
+    // Get the original Spacepoints.
+    art::Handle< std::vector<recob::SpacePoint> > sppth;
+    evt.getByLabel(fSpptModuleLabel, sppth);
+    for (size_t ii=0; ii<sppth->size() ;ii++)
+      scol->push_back(sppth->at(ii));
 
 
     // Find the best match for each track's Head and Tail.
@@ -188,6 +199,22 @@ namespace trkf {
 	const art::PtrVector<recob::SpacePoint>& sppts(GetSpacePointsFromComponentTracks(tvcol->at(ii), evt));
 	// Now make the Assns of relevant Sppts to stitched Track
 	util::CreateAssn(*this, evt, *tcol, sppts, *tsptassn, ii);
+	// Now Assns of sppts to hits
+
+	const art::PtrVector<recob::Hit>& hitsFromSppts(GetHitsFromAssdSpacePoints(sppts, evt));
+	for ( size_t jj=0; jj<sppts.size(); jj++ ) 
+	  {
+	    // find jjth sppt in the list of scol. Meaning, find kkth element of sppth.
+	    size_t ll(2e5);
+	    for ( size_t kk=0; kk<scol->size(); kk++ ) 
+	      {
+		art::Ptr<recob::SpacePoint> sppthOne (sppth,kk);
+		if ( sppthOne != sppts.at(jj)) continue;
+		ll = kk;
+	      }
+	    if (ll!=2e5)
+	      util::CreateAssn(*this, evt, *scol, hitsFromSppts, *spthassn, ll);
+	  }
       }
 
 
@@ -197,6 +224,7 @@ namespace trkf {
     // Add Hit-to-Track and Sppt-to-Track Assns.
     evt.put(std::move(thassn));
     evt.put(std::move(tsptassn));
+    evt.put(std::move(spthassn));
 
   }
   
@@ -229,6 +257,20 @@ namespace trkf {
     
     //    const art::PtrVector<recob::Hit> chits(hits);
     return sppts;
+  }
+
+  const art::PtrVector<recob::Hit> TrackStitcher::GetHitsFromAssdSpacePoints(const art::PtrVector<recob::SpacePoint> &sppts, const art::Event& evtGHFCT) 
+  {
+
+    art::PtrVector<recob::Hit> hits;
+    art::FindManyP<recob::Hit> hitAssns(sppts, evtGHFCT, fSpptModuleLabel); 
+
+    for (unsigned int ii=0; ii < sppts.size(); ++ii )
+      {
+	 hits.insert(hits.end(),hitAssns.at(ii).begin(), hitAssns.at(ii).end());
+      }
+
+    return hits;
   }
 
   DEFINE_ART_MODULE(TrackStitcher)
