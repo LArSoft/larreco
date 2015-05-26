f2a938bbbcae14d2c1da508608d136b947d5ee1a
Robert Sulej <rsulej@lbnegpvm01.fnal.gov>
2015-05-26 16:49:12 -0500

    add module for PMAlg

---
+++ TrackFinder/PMAlgTrackMaker_module.cc
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       PMAlgTrackMaker
+// Module Type: producer
+// File:        PMAlgTrackMaker_module.cc
+// Author:      D.Stefan (Dorota.Stefan@ncbj.gov.pl) and R.Sulej (Robert.Sulej@cern.ch), May 2015
+//
+// Makes 3D tracks with the Projection Matching Algorithm.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include <memory>
+
+namespace trkf {
+
+class PMAlgTrackMaker : public art::EDProducer {
+public:
+  explicit PMAlgTrackMaker(fhicl::ParameterSet const & p);
+
+  PMAlgTrackMaker(PMAlgTrackMaker const &) = delete;
+  PMAlgTrackMaker(PMAlgTrackMaker &&) = delete;
+  PMAlgTrackMaker & operator = (PMAlgTrackMaker const &) = delete;
+  PMAlgTrackMaker & operator = (PMAlgTrackMaker &&) = delete;
+
+  void produce(art::Event & e) override;
+
+
+private:
+
+};
+
+
+PMAlgTrackMaker::PMAlgTrackMaker(fhicl::ParameterSet const & p)
+// :
+// Initialize member data here.
+{
+  // Call appropriate produces<>() functions here.
+}
+
+void PMAlgTrackMaker::produce(art::Event & e)
+{
+  // Implementation of required member function here.
+}
+
+DEFINE_ART_MODULE(PMAlgTrackMaker)
+
+} // namespace trkf
