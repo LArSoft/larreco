6be143f1f16e6a6fae69f5c017f5205cc5ed0183
Lynn Garren <garren@fnal.gov>
2015-07-16 11:49:02 -0500

    initialize some local variables to zero

--- TrackFinder/CCTrackMaker_module.cc
+++ TrackFinder/CCTrackMaker_module.cc
@@ -1910,7 +1910,7 @@ namespace trkf {
     
     unsigned short ipl, icc, jpl, kpl, ii, jj, kk, icl, jcl, kcl, iend, jend, kend;
     short idir, jdir, kdir;
-    float islp, jslp, kslp, kAng, sigmaA, matchErr;
+    float islp=0., jslp=0, kslp=0., kAng=0., sigmaA=0., matchErr=0.;
     
     prt = (fDebugPlane >= 0);
     
