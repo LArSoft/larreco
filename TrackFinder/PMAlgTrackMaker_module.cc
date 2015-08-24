a2488cb032c02d9d9a9879f7bba8a3480cbf3b8b
Robert Sulej <rsulej@lbnegpvm09.fnal.gov>
2015-08-24 10:38:39 -0500

    do not use backtracker on real data

--- TrackFinder/PMAlgTrackMaker_module.cc
+++ TrackFinder/PMAlgTrackMaker_module.cc
@@ -162,6 +162,7 @@ private:
   double validate(pma::Track3D& trk, unsigned int testView);
   recob::Track convertFrom(const pma::Track3D& src);
 
+  bool fIsRealData;
   bool isMcStopping(void) const; // to be moved to the testing module
   int getMcPdg(const pma::Track3D& trk) const; // to be moved to the testing module
   // ------------------------------------------------------
@@ -383,25 +384,33 @@ recob::Track PMAlgTrackMaker::convertFrom(const pma::Track3D& src)
 
 bool PMAlgTrackMaker::isMcStopping(void) const
 {
-	art::ServiceHandle< cheat::BackTracker > bt;
-	const sim::ParticleList& plist = bt->ParticleList();
-	const simb::MCParticle* particle = plist.Primary(0);
-
-	if (particle)
+	if (fIsRealData)
 	{
-//		std::cout << "...:SIM:... " << particle->EndProcess()
-//			<< " n:" << particle->NumberDaughters()
-//			<< " m:" << particle->Mass()
-//			<< " E:" << particle->EndE() << std::endl;
-		return (particle->EndE() - particle->Mass() < 0.001);
-		//return (particle->NumberDaughters() == 0);
+		// maybe possible to apply check here
+		return false;
 	}
-	else return false;
+	else
+	{
+		art::ServiceHandle< cheat::BackTracker > bt;
+		const sim::ParticleList& plist = bt->ParticleList();
+		const simb::MCParticle* particle = plist.Primary(0);
+
+		if (particle)
+		{
+		//	std::cout << "...:SIM:... " << particle->EndProcess()
+		//		<< " n:" << particle->NumberDaughters()
+		//		<< " m:" << particle->Mass()
+		//		<< " E:" << particle->EndE() << std::endl;
+			return (particle->EndE() - particle->Mass() < 0.001);
+		//	return (particle->NumberDaughters() == 0);
+		}
+		else return false;
+	}
 }
 
 int PMAlgTrackMaker::getMcPdg(const pma::Track3D& trk) const
 {
-	// return 0; // would be good in case of real data...
+	if (fIsRealData) return 0;
 
 	art::ServiceHandle< cheat::BackTracker > bt;
 	std::map< int, size_t > pdg_counts;
@@ -845,6 +854,7 @@ bool PMAlgTrackMaker::sortHits(const art::Event& evt)
 void PMAlgTrackMaker::produce(art::Event& evt)
 {
 	fEvNumber = evt.id().event();
+	fIsRealData = evt.isRealData();
 
 	std::vector< pma::Track3D* > result;
 
