07b9eec46d6266f1d9e4045643e0ab326de16de8
Mike Wallbank <m.wallbank@sheffield.ac.uk>
2015-11-16 10:40:23 -0600

    MW: updates to clustering and showering modules.  Made the shower energy determination an alg so it can be used externally.  Added a new alg to assist in separation of track and shower objects.  Found the conversion from charge to energy for the DUNE fd.

---
+++ RecoAlg/ShowerEnergyAlg.cxx
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////
+// Class: ShowerEnergyAlg
+// File:  ShowerEnergyAlg.cxx
+// Author: Mike Wallbank (m.wallbank@sheffield.ac.uk), November 2015
+//
+// Shower energy finding class
+////////////////////////////////////////////////////////////////////////
+
+#include "RecoAlg/ShowerEnergyAlg.h"
+
+shower::ShowerEnergyAlg::ShowerEnergyAlg(fhicl::ParameterSet const& pset) {
+  fUGradient  = pset.get<double>("UGradient");
+  fUIntercept = pset.get<double>("UIntercept");
+  fVGradient  = pset.get<double>("VGradient");
+  fVIntercept = pset.get<double>("VIntercept");
+  fZGradient  = pset.get<double>("ZGradient");
+  fZIntercept = pset.get<double>("ZIntercept");
+}
+
+double shower::ShowerEnergyAlg::ShowerEnergy(art::PtrVector<recob::Hit> const& hits, int plane) {
+
+  /// Finds the total energy deposited by the shower in this view
+
+  double totalCharge = 0, totalEnergy = 0;
+
+  for (art::PtrVector<recob::Hit>::const_iterator hit = hits.begin(); hit != hits.end(); ++hit)
+    totalCharge += ((*hit)->Integral() * TMath::Exp((500 * (*hit)->PeakTime())/3e6));
+
+  switch (plane) {
+  case 0:
+    totalEnergy = (double)(totalCharge - fUIntercept)/(double)fUGradient;
+    break;
+  case 1:
+    totalEnergy = (double)(totalCharge - fVIntercept)/(double)fVGradient;
+    break;
+  case 2:
+    totalEnergy = (double)(totalCharge - fZIntercept)/(double)fZGradient;
+    break;
+  }
+
+  return totalEnergy;
+
+}
