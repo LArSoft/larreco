c8fcc3f7bd94713ded9c304487f2280112959338
Hamlet <_hamlet@libero.it>
2016-03-09 18:45:40 -0600

    Moved TrackUtils.h into lardata (lardata/RecoBaseArt)

--- larreco/RecoAlg/EMShowerAlg.cxx
+++ larreco/RecoAlg/EMShowerAlg.cxx
@@ -10,7 +10,7 @@
 
 #include "larreco/RecoAlg/EMShowerAlg.h"
 
-#include "larreco/RecoAlg/TrackUtils.h" // lar::TrackPitchInView()
+#include "lardata/RecoBaseArt/TrackUtils.h" // lar::utils::TrackPitchInView()
 
 #include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
 
@@ -398,7 +398,7 @@ double shower::EMShowerAlg::FinddEdx(std::vector<art::Ptr<recob::Hit> > const& t
 
   // Get the pitch
   double pitch = 0;
-  try { pitch = lar::TrackPitchInView(*track, trackHits.at(0)->View()); }
+  try { pitch = lar::utils::TrackPitchInView(*track, trackHits.at(0)->View()); }
   catch(...) { pitch = 0; }
 
   // Deal with large pitches
