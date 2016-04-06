36de9905ec9ab217494080b351d7a28f771c45ea
Bruce Baller <baller@mac-126574.dhcp.fnal.gov>
2016-04-06 15:34:46 -0500

    Defines AlgBitNames

---
+++ larreco/RecoAlg/TCAlg/DataStructs.cxx
@@ -0,0 +1,28 @@
+#include "larreco/RecoAlg/TCAlg/DataStructs.h"
+
+namespace tca {
+  const std::vector<std::string> AlgBitNames {
+    "MaskedHits",
+    "Kink",
+    "CWKink",
+    "CWStepChk",
+    "DeltaRay?",
+    "ModifyShortTj",
+    "TryNextPass",
+    "RevProp",
+    "Recovery1",
+    "Recovery2",
+    "ManyHitsAdded",
+    "SplitTraj",
+    "Comp3DVx",
+    "HiEndDelta",
+    "Hammer2DVx",
+    "JunkTj",
+    "Killed",
+    "StopAtVtx",
+    "Merged",
+    "TrimHits",
+    "UseHiMultEndHits"
+  };
+} // namespace tca
+
