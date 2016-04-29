4e06117b855bbfa1fa811d33184dfe7712cddf2e
Robert Sulej <robert@localhost.localdomain>
2016-04-29 09:02:43 -0500

    move our earlier scripts here, no downcale in python now (moved to larsoft module)

---
+++ larreco/RecoAlg/ImagePatternAlgs/scripts/train_cnn.py
@@ -0,0 +1,106 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(2017)  # for reproducibility
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+from utils import get_data, read_config, prepare_signal
+
+def save_model(model, name):
+    try:
+        with open(name + '_architecture.json', 'w') as f:
+            f.write(model.to_json())
+        model.save_weights(name + '_weights.h5', overwrite=True)
+        return True  # Save successful
+    except:
+        print 'save failed' #sys.exc_info()  # Prints exceptions
+        return False  # Save failed
+
+TOTAL_EVENTS = 100
+TEST_EVENTS_SPLIT = 70
+
+_, CNN_INPUT_DIR, PATCH_SIZE = read_config()
+
+db, db_y = None, None
+test_split = 0
+for evt_no in range(1,TOTAL_EVENTS+1):
+    tmp_db   = np.load(CNN_INPUT_DIR+'/db_evt_'+str(evt_no)+'.npy')
+    tmp_db_y = np.load(CNN_INPUT_DIR+'/db_y_evt_'+str(evt_no)+'.npy')
+
+    if db is None:
+        db = tmp_db
+        db_y = tmp_db_y
+    else:
+        db   = np.concatenate((db, tmp_db))
+        db_y = np.concatenate((db_y, tmp_db_y))
+    if evt_no == TEST_EVENTS_SPLIT:
+        test_split = db.shape[0]
+print 'Total data', db.shape, db_y.shape, 'test_split', test_split
+print 'Tracks', np.sum(db_y == 1), 'showers', np.sum(db_y == 0)
+
+# very ugly ADC scaling
+db = prepare_signal(db)
+
+# split between tran and test sets
+X_train = db[:test_split]
+X_test  = db[test_split:]
+y_train = db_y[:test_split]
+y_test  = db_y[test_split:]
+print 'Train', X_train.shape, 'test', X_test.shape
+
+
+batch_size = 128 #256
+nb_classes = 2
+nb_epoch = 3 #100
+# input image dimensions
+img_rows, img_cols = PATCH_SIZE, PATCH_SIZE
+# number of convolutional filters to use
+nb_filters = 64
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 5
+
+X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+X_train = X_train.astype("float32")
+X_test = X_test.astype("float32")
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# CNN definition
+model = Sequential()
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='valid',
+                        input_shape=(1, img_rows, img_cols)))
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+# dense layers
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              verbose=1, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+#save_model(model, '../cnn_models/run70tr30tst100epoch')
+
---
+++ larreco/RecoAlg/ImagePatternAlgs/scripts/utils.py
@@ -0,0 +1,123 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.pylab import *
+from os import listdir
+from os.path import isfile, join
+import os, json
+from scipy import ndimage
+from skimage.measure import block_reduce
+
+def get_event_bounds(A):
+    # get center with 99% of signal
+    cum = np.cumsum(np.sum(A, axis=0))
+    start_ind = np.where(cum > cum[-1]*0.005)[0][0]
+    end_ind = np.where(cum > cum[-1]*0.995)[0][0]
+    return start_ind, end_ind
+
+def get_data(fname):
+    print 'Reading', fname + '.raw'
+    A_raw     = np.genfromtxt(fname + '.raw', delimiter=' ')
+    A_deposit = np.genfromtxt(fname + '.deposit', delimiter=' ')
+    A_pdg     = np.genfromtxt(fname + '.pdg', delimiter=' ')
+
+    print np.sum(A_raw), np.sum(A_deposit), np.sum(A_pdg)
+    #assert np.sum(A_deposit) > 0
+    # get main event body (99% signal)
+    evt_start_ind, evt_stop_ind = get_event_bounds(A_raw)
+    print evt_start_ind, evt_stop_ind
+    A_raw     = A_raw[:,evt_start_ind:evt_stop_ind]
+    A_deposit = A_deposit[:,evt_start_ind:evt_stop_ind]
+    A_pdg     = np.abs(A_pdg[:,evt_start_ind:evt_stop_ind]) # ABS
+
+    # ugly fix
+    deposit_th_ind = A_deposit < 2.0e-5
+    #deposit_th_ind = A_raw < 4.0
+    A_pdg[deposit_th_ind] = 0
+    tracks = A_pdg.copy()
+    showers = A_pdg.copy()
+    tracks[A_pdg == 11] = 0
+    tracks[tracks > 0]   = 1
+
+    showers[A_pdg != 11] = 0
+    showers[showers > 0] = 1
+
+    return A_raw, A_deposit, A_pdg, tracks, showers
+
+
+def read_config():
+    config = None
+    with open('config.json', 'r') as fin:
+        config = json.loads(fin.read());
+    if config is None:
+        print 'This script requires configuration file: config.json'
+        exit(1)
+    INPUT_DIR = config['prepare_data']['input_dir']
+    OUTPUT_DIR = config['prepare_data']['output_dir']
+    PATCH_SIZE = config['prepare_data']['patch_size']
+    return INPUT_DIR, OUTPUT_DIR, PATCH_SIZE
+
+def prepare_signal(db):
+    db[db < 0.0] = 0.0
+    db[db > 70] = 70.0
+    db /= 10.0
+    return db
+
+def main(argv):
+
+    print '#'*50,'\nPrepare data for CNN'
+    INPUT_DIR, OUTPUT_DIR, PATCH_SIZE = read_config()
+    print 'Using %s as input dir, and %s as output dir' % (INPUT_DIR, OUTPUT_DIR)
+    print '#'*50
+
+    files = [f for f in os.listdir(INPUT_DIR) if 'tpc_2.raw' in f]
+    for fname in files:
+        fname = fname[:-4] # only main part of file, without extension
+        evt_no = fname.split('_')[2]
+        print 'Process', fname, 'EVT', evt_no
+
+        raw, deposit, pdg, tracks, showers = get_data(INPUT_DIR+'/'+fname)
+        total_learning_patches = int(np.sum(tracks) + np.sum(showers))
+        print 'Tracks', np.sum(tracks), 'showers', np.sum(showers)
+        print 'Total learning patches', total_learning_patches
+
+        db = np.zeros((total_learning_patches, PATCH_SIZE, PATCH_SIZE))
+        db_y = np.zeros((total_learning_patches, 1))
+
+        cnt_ind = 0
+        for i in range(raw.shape[0]):
+            for j in range(raw.shape[1]):
+                target = -1
+                if tracks[i,j] == 1:
+                    target = 1
+                elif showers[i,j] == 1:
+                    target = 0
+                if target == -1:
+                    continue
+
+                patch = np.zeros((PATCH_SIZE, PATCH_SIZE))
+                x_start = np.max([0, i - PATCH_SIZE/2])
+                x_stop  = np.min([raw.shape[0], x_start + PATCH_SIZE])
+
+                y_start = np.max([0, j - PATCH_SIZE/2])
+                y_stop  = np.min([raw.shape[1], y_start + PATCH_SIZE])
+
+                if x_stop - x_start != PATCH_SIZE or y_stop - y_start != PATCH_SIZE:
+                    continue
+                #print '#',i,j, target, raw.shape
+                #print x_start, x_stop
+                #print y_start, y_stop
+                db[cnt_ind] = raw[x_start:x_stop, y_start:y_stop]
+                db_y[cnt_ind] = target
+                cnt_ind += 1
+
+        print 'Added', cnt_ind, 'of total expected', total_learning_patches
+
+        np.save(OUTPUT_DIR+'/db_evt_' + evt_no, db[:cnt_ind])
+        np.save(OUTPUT_DIR+'/db_y_evt_' + evt_no, db_y[:cnt_ind])
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
