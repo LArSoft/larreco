4d08b12c60a26744970554377e2fb6e74213e703
Robert Sulej <robert@localhost.localdomain>
2016-06-14 04:59:12 -0500

    correct label names and vtx flags

--- larreco/RecoAlg/ImagePatternAlgs/PointIdAlg/PointIdAlg.h
+++ larreco/RecoAlg/ImagePatternAlgs/PointIdAlg/PointIdAlg.h
@@ -200,7 +200,7 @@ public:
 		kNone = 0,
 		kNuNC = 0x0010000, kNuCC = 0x0020000,                      // nu interaction type
 		kNuE  = 0x0100000, kNuMu = 0x0200000, kNuTau = 0x0400000,  // nu flavor
-		kHadr = 0x100000,   // hadronic inelastic scattering
+		kHadr = 0x1000000,  // hadronic inelastic scattering
 		kPi0  = 0x2000000,  // pi0 produced in this vertex
 		kDecay = 0x4000000, // particle decay at rest point
 		kConv = 0x8000000   // gamma conversion
