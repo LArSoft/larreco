88d6c880a0a859cf9a57affe4e85ef459452db85
Robert Sulej <robert.sulej@cern.ch>
2016-07-28 04:42:50 -0500

    add module to tag em-like clusters, for the moment similar behaviour to the track-shower splitter, so only em-like clusters (tagged) are outputed; will output also track-like clusters but need to prepare for such behaviour in tracking (pma); now only clusters with associated hits are taken at input, will add tagging for single (unclustered) hits asap

---
+++ larreco/RecoAlg/ImagePatternAlgs/EmTrackClusterId_module.cc
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Class:       EmTrackClusterId
+// Module Type: producer
+// File:        EmTrackClusterId_module.cc
+// Authors:     dorota.stefan@cern.ch pplonski86@gmail.com robert.sulej@cern.ch
+/////////////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "canvas/Utilities/InputTag.h"
+// #include "fhiclcpp/types/Atom.h"
+// #include "fhiclcpp/types/Sequence.h"
+// #include "fhiclcpp/types/Table.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "larcore/Geometry/Geometry.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "lardata/Utilities/AssociationUtil.h"
+
+#include "larreco/RecoAlg/ImagePatternAlgs/PointIdAlg/PointIdAlg.h"
+
+#include <memory>
+
+namespace nnet {
+
+class EmTrackClusterId : public art::EDProducer {
+public:
+
+/*  CalorimetryAlg has no fcl validation, so cannot make it for PointIdAlg and here as well...
+	struct Config {
+		using Name = fhicl::Name;
+		using Comment = fhicl::Comment;
+
+		fhicl::Table<nnet::PointIdAlg::Config> PointIdAlg {
+			Name("PointIdAlg")
+		};
+
+		fhicl::Atom<art::InputTag> ClusterModuleLabel {
+			Name("ClusterModuleLabel"),
+			Comment("tag of clusters which are to be EM/track tagged")
+		};
+
+		fhicl::Atom<double> Threshold {
+			Name("Threshold"),
+			Comment("tag cluster as EM-like if net output accumulated over hits > threshold")
+		};
+
+		fhicl::Sequence<int> Views {
+			Name("Views"),
+			Comment("tag clusters in selected views only, or in all views if empty list")
+		};
+	};
+	using Parameters = art::EDProducer::Table<Config>;
+	explicit EmTrackClusterId(Parameters const & p);
+*/
+
+	explicit EmTrackClusterId(fhicl::ParameterSet const & p);
+
+	EmTrackClusterId(EmTrackClusterId const &) = delete;
+	EmTrackClusterId(EmTrackClusterId &&) = delete;
+	EmTrackClusterId & operator = (EmTrackClusterId const &) = delete;
+	EmTrackClusterId & operator = (EmTrackClusterId &&) = delete;
+
+	void produce(art::Event & e) override;
+
+private:
+
+	PointIdAlg fPointIdAlg;
+
+	art::InputTag fClusterModuleLabel;
+
+	double fThreshold;
+
+	std::vector< int > fViews;
+};
+// ------------------------------------------------------
+
+EmTrackClusterId::EmTrackClusterId(fhicl::ParameterSet const & p) :
+	fPointIdAlg(p.get< fhicl::ParameterSet >("PointIdAlg")),
+	fClusterModuleLabel(p.get< art::InputTag >("ClusterModuleLabel")),
+	fThreshold(p.get< double >("Threshold")),
+	fViews(p.get< std::vector< int > >("Views"))
+
+{
+	produces< std::vector<recob::Cluster> >();
+	produces< art::Assns<recob::Cluster, recob::Hit> >();
+}
+// ------------------------------------------------------
+
+void EmTrackClusterId::produce(art::Event & evt)
+{
+	auto clusters = std::make_unique< std::vector< recob::Cluster > >();
+	auto clu2hit = std::make_unique< art::Assns< recob::Cluster, recob::Hit > >();
+
+	art::Handle< std::vector<recob::Cluster> > cluListHandle;
+	if (!evt.getByLabel(fClusterModuleLabel, cluListHandle))
+	{
+		mf::LogError("EmTrackClusterId") << "Clusters not found in the event.";
+		return;
+	}
+
+	mf::LogVerbatim("EmTrackClusterId") << "next event: " << evt.run() << " / " << evt.id().event();
+
+	unsigned int cidx = 0;
+	const unsigned int emTag = 0x10000;
+
+	art::FindManyP< recob::Hit > hitsFromClusters(cluListHandle, evt, fClusterModuleLabel);
+	for (size_t i = 0; i < hitsFromClusters.size(); ++i)
+	{
+		auto v = hitsFromClusters.at(i);
+		if (v.empty()) continue;
+
+		int view = v.front()->View();
+		int tpc = v.front()->WireID().TPC;
+		int cryo = v.front()->WireID().Cryostat;
+
+		if (!fViews.empty())
+		{
+			bool skip = true;
+			for (auto k : fViews) if (k == view) { skip = false; break; }
+			if (skip) continue;
+		}
+
+		// better if clusters are stored as sorted by tpc/view, otherwise this can be costful call:
+		fPointIdAlg.setWireDriftData(evt, view, tpc, cryo);
+
+		// anyway this is where we spend most time:
+		auto vout = fPointIdAlg.predictIdVector(v);
+		float pvalue = vout[0] / (vout[0] + vout[1]);
+
+		mf::LogVerbatim("EmTrackClusterId") << "tpc:" << tpc << " view:" << view << " size:" << v.size() << " p:" << pvalue;
+
+		if (pvalue > fThreshold) continue; // identified as track-like, for the moment we save only em-like parts
+
+		clusters->emplace_back(
+			recob::Cluster(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
+				v.size(), 0.0F, 0.0F, cidx + emTag, (geo::View_t)v.front()->View(), v.front()->WireID().planeID()));
+		util::CreateAssn(*this, evt, *clusters, v, *clu2hit);
+		cidx++;
+	}
+
+	evt.put(std::move(clusters));
+	evt.put(std::move(clu2hit));
+}
+// ------------------------------------------------------
+
+DEFINE_ART_MODULE(EmTrackClusterId)
+
+}
+
