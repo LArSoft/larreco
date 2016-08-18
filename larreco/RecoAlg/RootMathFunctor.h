5bf76f3b06ee2df55f07e96372821125e3a71d4b
Lynn Garren <garren@fnal.gov>
2016-08-18 13:55:37 -0500

    replacing old RootMathFunctor.h with modern quiet_Math_Functor.h

--- larreco/RecoAlg/RootMathFunctor.h
+++ larreco/RecoAlg/RootMathFunctor.h
@@ -1,7 +1,4 @@
 // wrap the root Math/Functor.h package so it does not complain about
 // the deprecated auto_ptr
 
-#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)) && !defined(__INTEL_COMPILER) 
-# pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-#include "Math/Functor.h"
+#include "larcore/CoreUtils/quiet_Math_Functor.h" // remove the wrapper when ROOT header is fixed
