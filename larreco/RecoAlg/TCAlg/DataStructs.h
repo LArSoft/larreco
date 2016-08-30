37617fd5a37159507f59fe7a8450f627419533b1
Bruce Baller <baller@mac-126574.local>
2016-08-30 15:48:16 -0500

    Re-arrange tjs. Add newHits

--- larreco/RecoAlg/TCAlg/DataStructs.h
+++ larreco/RecoAlg/TCAlg/DataStructs.h
@@ -187,21 +187,24 @@ namespace tca {
   extern const std::vector<std::string> AlgBitNames;
   
   struct TjStuff {
+    // These variables don't change in size from event to event
+    float UnitsPerTick;     ///< scale factor from Tick to WSE equivalent units
+    std::vector<unsigned int> NumWires;
+    std::vector<float> MaxPos0;
+    std::vector<float> MaxPos1;
+    std::vector<unsigned int> FirstWire;    ///< the first wire with a hit
+    std::vector<unsigned int> LastWire;      ///< the last wire with a hit
+    // The variables below do change in size from event to event
     std::vector<Trajectory> allTraj; ///< vector of all trajectories in each plane
-    std::vector<short> inTraj;       ///< Hit -> trajectory ID (0 = unused)
     std::vector<art::Ptr<recob::Hit>> fHits;
+    std::vector<short> inTraj;       ///< Hit -> trajectory ID (0 = unused)
+    std::vector<recob::Hit> newHits;
     // vector of pairs of first (.first) and last+1 (.second) hit on each wire
     // in the range fFirstWire to fLastWire. A value of -2 indicates that there
     // are no hits on the wire. A value of -1 indicates that the wire is dead
     std::vector<std::vector< std::pair<int, int>>> WireHitRange;
     // vector of pointers to wires in each plane
     std::vector<std::vector<art::Ptr<recob::Wire>>> WirePtr;
-    std::vector<unsigned int> NumWires;
-    std::vector<float> MaxPos0;
-    std::vector<float> MaxPos1;
-    float UnitsPerTick;     ///< scale factor from Tick to WSE equivalent units
-    std::vector<unsigned int> FirstWire;    ///< the first wire with a hit
-    std::vector<unsigned int> LastWire;      ///< the last wire with a hit
     std::vector<short> inClus;    ///< Hit -> cluster ID (0 = unused)
     std::vector< ClusterStore > tcl; ///< the clusters we are creating
     std::vector< VtxStore > vtx; ///< 2D vertices
