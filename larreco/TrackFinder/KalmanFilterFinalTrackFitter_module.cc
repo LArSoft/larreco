2e4f9b684478dcd7dabb7356d23899241209e597
Giuseppe Cerati <cerati@fnal.gov>
2016-11-05 22:10:39 -0500

    improved use of fcl file parameters

--- larreco/TrackFinder/KalmanFilterFinalTrackFitter_module.cc
+++ larreco/TrackFinder/KalmanFilterFinalTrackFitter_module.cc
@@ -18,6 +18,8 @@
 
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Table.h"
 #include "canvas/Utilities/InputTag.h"
 #include "canvas/Persistency/Common/FindManyP.h"
 
@@ -39,61 +41,104 @@
 
 #include <memory>
 
-class KalmanFilterFinalTrackFitter;
+namespace trkf {
+  
+  class KalmanFilterFinalTrackFitter : public art::EDProducer {
+  public:
 
-class KalmanFilterFinalTrackFitter : public art::EDProducer {
-public:
-  explicit KalmanFilterFinalTrackFitter(fhicl::ParameterSet const & p);
-  ~KalmanFilterFinalTrackFitter();
+    struct Inputs {
+      using Name = fhicl::Name;
+      using Comment = fhicl::Comment;
 
-  // Plugins should not be copied or assigned.
-  KalmanFilterFinalTrackFitter(KalmanFilterFinalTrackFitter const &) = delete;
-  KalmanFilterFinalTrackFitter(KalmanFilterFinalTrackFitter &&) = delete;
-  KalmanFilterFinalTrackFitter & operator = (KalmanFilterFinalTrackFitter const &) = delete;
-  KalmanFilterFinalTrackFitter & operator = (KalmanFilterFinalTrackFitter &&) = delete;
+      fhicl::Atom<art::InputTag> inputTracksLabel {
+	Name("inputTracksLabel"),
+	Comment("Label of reco::Track Collection to be fit")
+      };
+      fhicl::Atom<art::InputTag> inputCaloLabel {
+	Name("inputCaloLabel"),
+	Comment("Label of anab::Calorimetry Collection, matching inputTracksLabel, to be used for initial momentum estimate. Used only if momFromCalo is set to true.")
+      };
+      fhicl::Atom<art::InputTag> inputMCLabel {
+	Name("inputMCLabel"),
+	Comment("Label of sim::MCTrack Collection to be used for initial momentum estimate. Used only if momFromMC is set to true and momFromCalo is set to false.")
+      };
+    };
 
-  void produce(art::Event & e) override;
+    struct Options {
+      using Name = fhicl::Name;
+      using Comment = fhicl::Comment;
 
-private:
-  std::string inputTracksLabel;
-  bool pFromCalo, pFromMC;
-  std::string inputCaloLabel;
-  std::string inputMCLabel;
-  double pval;
-  int pdgId;
-  bool useRMS;
-  trkf::TrackKalmanFitter* kalmanFitter;
-  trkf::Propagator* prop;
-};
+      fhicl::Atom<bool> pFromCalo {
+	Name("momFromCalo"),
+	Comment("Flag used to get initial momentum estimate from inputCaloLabel collection. Has precedence over momFromMC.")
+      };
+      fhicl::Atom<bool> pFromMC {
+	Name("momFromMC"),
+	Comment("Flag used to get initial momentum estimate from inputMCLabel collection. Bypassed if momFromCalo is true.")
+      };
+      fhicl::Atom<double> pval {
+	Name("momentumInGeV"),
+	Comment("Fixed momentum estimate value, to be used when momFromCalo and momFromMC are both false, or if the estimate is not available.")
+      };
+      fhicl::Atom<int> pdgId {
+	Name("pdgId"),
+	Comment("PDG id hypothesis to be used during the fit.")
+      };
+      fhicl::Atom<bool> useRMS {
+	Name("useRMSError"),
+	Comment("Flag to replace the default hit error SigmaPeakTime() with RMS().")
+      };
+    };
+    
+    struct Config {
+      using Name = fhicl::Name;
+      
+      fhicl::Table<KalmanFilterFinalTrackFitter::Inputs> inputs {
+	Name("inputs"),
+      };  
+      fhicl::Table<KalmanFilterFinalTrackFitter::Options> options {
+	Name("options")
+      };
+    };
+    using Parameters = art::EDProducer::Table<Config>;
+    
+    explicit KalmanFilterFinalTrackFitter(Parameters const & p);
+    ~KalmanFilterFinalTrackFitter();
+    
+    // Plugins should not be copied or assigned.
+    KalmanFilterFinalTrackFitter(KalmanFilterFinalTrackFitter const &) = delete;
+    KalmanFilterFinalTrackFitter(KalmanFilterFinalTrackFitter &&) = delete;
+    KalmanFilterFinalTrackFitter & operator = (KalmanFilterFinalTrackFitter const &) = delete;
+    KalmanFilterFinalTrackFitter & operator = (KalmanFilterFinalTrackFitter &&) = delete;
+    
+    void produce(art::Event & e) override;
+    
+  private:
+    Parameters p_;
+    double pval;
+    trkf::TrackKalmanFitter* kalmanFitter;
+    trkf::Propagator* prop;
+  };
+}
 
-KalmanFilterFinalTrackFitter::KalmanFilterFinalTrackFitter(fhicl::ParameterSet const & p)
-  : inputTracksLabel(p.get<std::string>("inputTracksLabel"))
+trkf::KalmanFilterFinalTrackFitter::KalmanFilterFinalTrackFitter(trkf::KalmanFilterFinalTrackFitter::Parameters const & p)
+  : p_(p)
 {
-  pFromCalo = p.get<bool>("momFromCalo");
-  pFromMC = p.get<bool>("momFromMC");
-  if (pFromCalo) {
-    inputCaloLabel = p.get<std::string>("inputCaloLabel");
-  } else if (pFromMC) {
-    inputMCLabel = p.get<std::string>("inputMCLabel");
-  } else {
-    pval = p.get<double>("momentumInGeV");
-  }
-  pdgId = p.get<int>("pdgId");
-  useRMS = p.get<bool>("useRMSError");
+  pval = p_().options().pval();
   
   prop = new trkf::PropYZPlane(0., false);
-  kalmanFitter = new trkf::TrackKalmanFitter(prop,useRMS);
+  kalmanFitter = new trkf::TrackKalmanFitter(prop,p_().options().useRMS());
 
   produces<std::vector<recob::Track> >();
   produces<art::Assns<recob::Track, recob::Hit> >();
 }
 
-KalmanFilterFinalTrackFitter::~KalmanFilterFinalTrackFitter() {
+trkf::KalmanFilterFinalTrackFitter::~KalmanFilterFinalTrackFitter() {
   delete prop;
   delete kalmanFitter;
 }
 
-void KalmanFilterFinalTrackFitter::produce(art::Event & e)
+void trkf::KalmanFilterFinalTrackFitter::produce(art::Event & e)
 {
   
   auto outputTracks = std::make_unique<std::vector<recob::Track> >();
@@ -102,17 +147,17 @@ void KalmanFilterFinalTrackFitter::produce(art::Event & e)
   auto const tid = getProductID<std::vector<recob::Track> >(e);
   auto const tidgetter = e.productGetter(tid);
   
-  art::InputTag TrackInputTag(inputTracksLabel);
+  art::InputTag TrackInputTag(p_().inputs().inputTracksLabel());
   art::ValidHandle<std::vector<recob::Track> > inputTracks = e.getValidHandle<std::vector<recob::Track> >(TrackInputTag);
   std::unique_ptr<art::FindManyP<recob::Hit> > trackHits(new art::FindManyP<recob::Hit>(inputTracks, e, TrackInputTag));
 
-  art::InputTag CaloInputTag(inputCaloLabel);
+  art::InputTag CaloInputTag(p_().inputs().inputCaloLabel());
   std::unique_ptr<art::FindManyP<anab::Calorimetry> > trackCalo;
-  if (pFromCalo) {
+  if (p_().options().pFromCalo()) {
     trackCalo = std::unique_ptr<art::FindManyP<anab::Calorimetry> >(new art::FindManyP<anab::Calorimetry>(inputTracks, e, CaloInputTag));
-  } else if (pFromMC) {
+  } else if (p_().options().pFromMC()) {
     //FIXME, eventually remove this (ok only for single particle MC)
-    art::InputTag SimTrackInputTag(inputMCLabel);
+    art::InputTag SimTrackInputTag(p_().inputs().inputMCLabel());
     art::ValidHandle<std::vector<sim::MCTrack>> simTracks = e.getValidHandle<std::vector<sim::MCTrack>>(SimTrackInputTag);
     for (unsigned int iMC = 0; iMC < simTracks->size(); ++iMC) {
       const sim::MCTrack& mctrack = simTracks->at(iMC);
@@ -130,7 +175,7 @@ void KalmanFilterFinalTrackFitter::produce(art::Event & e)
   for (unsigned int iTrack = 0; iTrack < inputTracks->size(); ++iTrack) {
 
     const recob::Track& track = inputTracks->at(iTrack);
-    if (pFromCalo) {
+    if (p_().options().pFromCalo()) {
       const std::vector<art::Ptr<anab::Calorimetry> >& calo = trackCalo->at(iTrack);
       double sumenergy = 0.;
       int nviews = 0.;
@@ -146,7 +191,7 @@ void KalmanFilterFinalTrackFitter::produce(art::Event & e)
 
     recob::Track outTrack;
     art::PtrVector<recob::Hit> outHits;
-    bool fitok = kalmanFitter->fitTrack(track, trackHits->at(iTrack), pval, pdgId, outTrack, outHits);
+    bool fitok = kalmanFitter->fitTrack(track, trackHits->at(iTrack), pval, p_().options().pdgId(), outTrack, outHits);
 
     if (!fitok) continue;
     
@@ -162,4 +207,4 @@ void KalmanFilterFinalTrackFitter::produce(art::Event & e)
   
 }
 
-DEFINE_ART_MODULE(KalmanFilterFinalTrackFitter)
+DEFINE_ART_MODULE(trkf::KalmanFilterFinalTrackFitter)
