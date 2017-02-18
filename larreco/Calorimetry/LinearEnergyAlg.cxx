7a83e250ca3c424a50a394a7c0974afddbaff363
Gianluca Petrillo <petrillo@fnal.gov>
2017-02-17 19:40:33 -0600

    First revision of the cluster energy algorithm draft.

--- larreco/Calorimetry/LinearEnergyAlg.cxx
+++ larreco/Calorimetry/LinearEnergyAlg.cxx
@@ -9,63 +9,164 @@
 
 // LArSoft libraries
 #include "larreco/Calorimetry/LinearEnergyAlg.h"
-#include "larcore/Geometry/GeometryCore.h"
+#include "lardata/Utilities/ForEachAssociatedGroup.h" // util::associated_groups()
 #include "larcoreobj/SimpleTypesAndConstants/PhysicalConstants.h"
 
-// infrastructure and utilities
-#include "cetlib/exception.h"
 
 // C/C++ standard libraries
 #include <stdexcept> // std::runtime_error()
-#include <memory> // std::make_unique()
+#include <cmath>
+
+
+namespace {
+  
+  /**
+   * @brief Returns the hits associated with the specified cluster.
+   * @param cluster the cluster to retrieve the associated hits of
+   * @param hitsPerCluster all the cluster-hit associations
+   * @return a range of hits, that can be used in a range-for loop
+   * @throw std::runtime_error if the cluster is not found
+   */
+  auto hitsAssociatedWith(
+    art::Ptr<recob::Cluster> const& cluster,
+    art::Assns<recob::Cluster, recob::Hit> const& hitsPerCluster
+    )
+  {
+    //
+    // Reminder: the cluster-hit association is a list of links:
+    //     
+    //     cluster #0  <--> hit #7
+    //     cluster #0  <--> hit #8
+    //     cluster #0  <--> hit #10
+    //     cluster #1  <--> hit #4
+    //     cluster #1  <--> hit #5
+    //     cluster #1  <--> hit #9
+    //     cluster #2  <--> hit #11
+    //     cluster #2  <--> hit #12
+    //     ...
+    //     
+    // associated_groups() presents this associations grouped by cluster:
+    //     
+    //     cluster #0  <--> hits { #7, #8, #10 }
+    //     cluster #1  <--> hits { #4, #5, #9 }
+    //     cluster #2  <--> hits { #11, #12 }
+    //     ...
+    //     
+    // This function loops through this latter list, looking for the cluster
+    // number that is stored in the `cluster` argument.
+    //
+    size_t iAssociatedCluster = 0;
+    for (auto hits: util::associated_groups(hitsPerCluster)) {
+      
+      // if the iAssociatedCluster is the index we are looking for, we are done:
+      if (iAssociatedCluster == cluster.key()) return hits;
+      
+      ++iAssociatedCluster; // else, try the next one!
+      
+    } // for all clusters
+    throw std::runtime_error
+      ("No hit associated with cluster " + std::to_string(cluster.key()) + " found!");
+  } // calo::LinearEnergyAlg::hitsAssociatedWith()
+  
+} // local namespace
+
+
+
+calo::LinearEnergyAlg::LinearEnergyAlg(Config const& config)
+  : fUseArea( config.UseArea() )
+  , fRecombFactor( 1. )
+  , fElectronLifetime( 1e10 ) // needs to be read from service
+  , fDeconNorm( 200 )
+{
+  if ( config.RecombinationModel() == "ModBox" ) fRecombModel = kModBox;
+  else if ( config.RecombinationModel() == "Birk" ) fRecombModel = kBirk;
+  else if ( config.RecombinationModel() == "Constant" ) fRecombModel = kConstant;
+  else {
+    throw std::runtime_error
+      ( "Unsupported recombination mode: '" + config.RecombinationModel() + "'" );
+  }
+}
+
 
 void calo::LinearEnergyAlg::initialize() {
 
   fElectronLifetime = detp->ElectronLifetime();
-  fDeconNorm = 200;
-  fRecombFactor = 1.;
 
 }
 
-void calo::LinearEnergyAlg::CalculateEnergy() {  // input clusters and hits, shower direction?
 
-  for ( auto const &) {  //loop clusters
-    for ( auto const& hit: hits ) {  // loop hits
 
-      // lifetime correction
-      double t = this->ConvertTickToDriftTime( hit->PeakTime(), hit->View() );
-      double LifetimeCorr = std::exp( t / fElectronLifetime );
+double calo::LinearEnergyAlg::CalculateHitEnergy(recob::Hit const& hit) const
+{
 
-      // hit charge (ADC) -> Coulomb -> Number of electrons -> eV
-      dE += hit->Integral() * kWion * fDeconNorm;
-      // dE * lifetime correction
-      dE *= LifetimeCorr;
-      // dE / recombination factor
-      double dEdx = 2.3;
-      double RecombCorr = this->RecombinationCorrection( dEdx ) * kWion / dEdx;
-      dE /= RecombCorr;
-    }
-  }
-  return;
-}
+  double const t = ConvertTickToDriftTime( hit.PeakTime(), hit.View() );
+  double const LifetimeCorr = std::exp( t / fElectronLifetime );
+  
+  // hit charge (ADC) -> Coulomb -> Number of electrons -> eV
+  double dE = hit.Integral() * kWion * fDeconNorm;
+  
+  // dE * lifetime correction
+  dE *= LifetimeCorr;
+  
+  // dE / recombination factor
+  double const dEdx = 2.3;
+  double const RecombCorr = RecombinationCorrection( dEdx ) * kWion / dEdx;
+  dE /= RecombCorr;
+  
+  return dE;
+  
+} // calo::LinearEnergyAlg::CalculateHitEnergy()
+
+
+
+std::vector<double> calo::LinearEnergyAlg::CalculateEnergy(
+  std::vector<art::Ptr<recob::Cluster>> const& clusters,
+  art::Assns<recob::Cluster, recob::Hit> const& hitsPerCluster
+  )
+{  // input clusters and hits, shower direction?
+  
+  std::vector<double> clusterEnergies;
+  
+  for (art::Ptr<recob::Cluster> const& cluster: clusters) {
+    
+    // search for the right association to the cluster we are processing
+    auto hits = hitsAssociatedWith(cluster, hitsPerCluster);
+    
+    double E = 0.0; // total cluster energy
+    
+    // hits are the hits associated to the cluster
+    for (art::Ptr<recob::Hit> const& hitPtr: hits) {
+      
+      E += CalculateHitEnergy(*hitPtr);
+      
+    } // for hits associated to the cluster
+    
+    clusterEnergies.push_back(E);
+    
+  } // for all clusters
+  
+  return clusterEnergies;
+  
+} // calo::LinearEnergyAlg::CalculateEnergy()
 
 // Get the electron drift time; may move to somewhere else
 double calo::LinearEnergyAlg::ConvertTickToDriftTime( double tick, geo::View_t plane ) const {
-  double offTick = detp->GetXTicksOffset( plane, 0, 0 ) - dept->TriggerOffset();
+  double offTick = detp->GetXTicksOffset( plane, 0, 0 ) - detp->TriggerOffset();
   return detc->TPCTick2Time( tick - offTick );
 }
 
-double calo::LinearEnergyAlg::RecombinationCorrection( double dEdx ) {
+double calo::LinearEnergyAlg::RecombinationCorrection( double dEdx ) const {
 
   switch ( fRecombModel ) {
-    case calo::kModBox:
+    case kModBox:
       return this->ModBoxInverse( dEdx );
-    case calo::kBirk:
+    case kBirk:
       return this->BirksInverse( dEdx );
-    case calo::kConstant:
+    case kConstant:
       return fRecombFactor;
     default:
-      return 1.;
+      throw std::logic_error
+        ("Unexpected! recombination model in RecombinationCorrection()");
   }
 }
 
@@ -90,7 +191,7 @@ double calo::LinearEnergyAlg::BirksInverse( double dEdx ) const {
   double A3t     = util::kRecombA;
   double K3t     = util::kRecombk;                     // in KV/cm*(g/cm^2)/MeV
   double rho     = detp->Density();                    // LAr density in g/cm^3
-  double Efield  = dept->Efield();                     // Electric Field in the drift region in KV/cm
+  double Efield  = detp->Efield();                     // Electric Field in the drift region in KV/cm
   K3t           /= rho;                                // KV/MeV
 
   double dQdx = ( A3t/kWion ) / ( K3t / Efield * dEdx + 1);
--- larreco/Calorimetry/LinearEnergyAlg.h
+++ larreco/Calorimetry/LinearEnergyAlg.h
@@ -13,10 +13,14 @@
 // LArSoft libraries
 #include "lardataobj/RecoBase/Cluster.h"
 #include "lardataobj/RecoBase/Hit.h"
-#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
-#include "lardata/DetectorInfoServices/DetectorClocksService.h"
+#include "lardata/DetectorInfo/DetectorProperties.h"
+#include "lardata/DetectorInfo/DetectorClocks.h"
+#include "larcore/Geometry/GeometryCore.h"
 
 // infrastructure and utilities
+// #include "cetlib/exception.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/Assns.h"
 #include "fhiclcpp/types/Comment.h"
 #include "fhiclcpp/types/Name.h"
 #include "fhiclcpp/types/Atom.h"
@@ -25,7 +29,6 @@
 
 // C/C++ standard libraries
 #include <vector>
-#include <memory> // std::unique_ptr<>
 
 namespace calo {
 
@@ -38,7 +41,7 @@ namespace calo {
       kBirk,
       kConstant,
       nRecombinationModel
-    }
+    };
 
     /// Algorithm configuration
     struct Config {
@@ -67,17 +70,9 @@ namespace calo {
      *
      * For the configuration, see `LinearEnergyAlg` documentation.
      */
-    LinearEnergyAlg(Config const& config)
-      : fUseArea( config.UseArea() ),
-        fRecombModel( config.RecombinationModel() ) {
-          if ( config.RecombinationModel() == "ModBox" ) fRecombModel = kModBox;
-          else if ( config.RecombinationModel() == "Birk" ) fRecombModel = kBirk;
-          else if ( config.RecombinationModel() == "Constant" ) fRecombModel = kConstant;
-          else {
-            throw std::runtime_error
-            ( "Unsupported recombination mode: '" + config.RecombinationModel() + "'" );
-          } {}
-
+    LinearEnergyAlg(Config const& config);
+    
+    
     /**
      * @brief Constructor with configuration validation
      * @param pset FHiCL configuration parameter set
@@ -108,7 +103,24 @@ namespace calo {
       { detp = &detproperty; detc = &detclock; geom = &geometry; initialize(); }
 
     /// @}
-
+    
+    /**
+     * @brief Calculates the energy of the shower
+     * @param clusters list of clusters we want the energy of
+     * @param hitsPerCluster associations of all clusters to all hits
+     * @return a vector with one energy per input cluster [GeV]
+     * 
+     * 
+     * @todo Describe the algorithm
+     * 
+     */
+    std::vector<double> CalculateEnergy(
+      std::vector<art::Ptr<recob::Cluster>> const& clusters,
+      art::Assns<recob::Cluster, recob::Hit> const& hitsPerCluster
+      );
+    
+    
+    
   private:
 
     /// Pointer to the geometry to be used
@@ -131,23 +143,31 @@ namespace calo {
     double fElectronLifetime;
     double fDeconNorm;
 
-    static const double kWion           = 23.6e-6;  ///< ionization potenial in LAr, 23.6 eV = 1e, Wion in MeV/e
+    static constexpr double kWion           = 23.6e-6;  ///< ionization potenial in LAr, 23.6 eV = 1e, Wion in MeV/e
     // Conversion for energy deposited in GeV to number of ionization electrons produced
-    static const double kRecombFactor   = 0.62;     ///< constant correction used in the current MicroBooNE shower reconstruction
+    static constexpr double kRecombFactor   = 0.62;     ///< constant correction used in the current MicroBooNE shower reconstruction
 
     void initialize();
-
-    void CalculateEnergy();
+    
+    /**
+     * @brief Returns the corrected energy from the hit.
+     * @param hit the hit to be corrected
+     * @return the corrected hit energy [GeV]
+     * 
+     * @todo document the algorithm here
+     * 
+     */
+    double CalculateHitEnergy(recob::Hit const& hit) const;
 
     double ConvertTickToDriftTime( double tick, geo::View_t plane ) const;
 
-    double RecombinationCorrection( double dEdx );  ///< TODO: make it more flexible
+    double RecombinationCorrection( double dEdx ) const;  ///< TODO: make it more flexible
 
     double ModBoxInverse( double dEdx ) const;
 
     double BirksInverse( double dEdx ) const;
 
-  }  // class LinearEnergyAlg
+  };  // class LinearEnergyAlg
 
 }  // namespace calo
 #endif  // LARRECO_CALORIMETRY_LINEARENERGYALG_H
