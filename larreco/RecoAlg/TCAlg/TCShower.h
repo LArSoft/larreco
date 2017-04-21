dfe5f7f77619031be1b81750657361dcf50c7ef2
baller <baller@fnal.gov>
2017-04-21 15:59:21 -0500

    Re-structure shower code.

--- larreco/RecoAlg/TCAlg/TCShower.h
+++ larreco/RecoAlg/TCAlg/TCShower.h
@@ -38,10 +38,9 @@ namespace tca {
   void FindShowers(TjStuff& tjs, const CTP_t& inCTP);
   void FillPts(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void DefineShower(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
-  bool RefineShowerTj(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  void RefineShowerTj(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   bool AddTj(TjStuff& tjs, unsigned short TjID, const unsigned short& cotIndex, bool doUpdate, bool prt);
   bool RemoveTj(TjStuff& tjs, unsigned short TjID, const unsigned short& cotIndex, bool doUpdate, bool prt);
-  bool UpdateShower(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   bool FindChargeCenter(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void FindAngle(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void FillRotPos(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
@@ -49,14 +48,16 @@ namespace tca {
   void FindExternalParent(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   float ParentFOM(TjStuff& tjs, Trajectory& tj, const unsigned short& tjEnd, ShowerStruct& ss, bool prt);
   void DefineEnvelope(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
-  bool AddTjsInsideEnvelope(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  void AddTjsInsideEnvelope(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   bool AddLooseHits(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void FindStartChg(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  std::vector<float> StartChgVec(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void DumpShowerPts(TjStuff& tjs, const unsigned short& cotIndex);
   
   void AddMissedTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<unsigned short>& tjl);
   void TagShowerTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<std::vector<unsigned short>>& tjList);
-  void MergeShowers(TjStuff& tjs, const CTP_t& inCTP, bool prt);
+  void MergeOverlap(TjStuff& tjs, const CTP_t& inCTP, bool prt);
+  void MergeSubShowers(TjStuff& tjs, const CTP_t& inCTP, bool prt);
   bool MergeShowersAndStore(TjStuff& tjs, unsigned short istj, unsigned short jstj, bool prt);
   void TransferTjHits(TjStuff& tjs, const CTP_t& inCTP, bool prt);
   unsigned short GetCotsIndex(TjStuff& tjs, const unsigned short& ShowerTjID);
