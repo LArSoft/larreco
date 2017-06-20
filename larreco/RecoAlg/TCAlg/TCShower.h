1750c56e530c0ce24bdc0a704b785a2d8f1a1b88
baller <baller@fnal.gov>
2017-06-20 08:54:11 -0500

    Ongoing development

--- larreco/RecoAlg/TCAlg/TCShower.h
+++ larreco/RecoAlg/TCAlg/TCShower.h
@@ -36,7 +36,8 @@ namespace tca {
   void FindShowers(TjStuff& tjs, const CTP_t& inCTP);
   void FillPts(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void DefineShower(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
-  void RefineShowerTj(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  void DefineShower(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  void Find3DVertex(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void MakeShowerObsolete(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   bool AddTj(TjStuff& tjs, unsigned short TjID, const unsigned short& cotIndex, bool doUpdate, bool prt);
   bool RemoveTj(TjStuff& tjs, unsigned short TjID, const unsigned short& cotIndex, bool doUpdate, bool prt);
@@ -49,13 +50,16 @@ namespace tca {
   float ParentFOM(TjStuff& tjs, Trajectory& tj, const unsigned short& tjEnd, ShowerStruct& ss, bool prt);
   void DefineEnvelope(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void AddTjsInsideEnvelope(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
+  void RefineShowerTj(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   bool AddLooseHits(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void FindStartChg(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   std::vector<float> StartChgVec(TjStuff& tjs, const unsigned short& cotIndex, bool prt);
   void DumpShowerPts(TjStuff& tjs, const unsigned short& cotIndex);
   
-  void AddMissedTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<unsigned short>& tjl);
-  void TagShowerTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<std::vector<unsigned short>>& tjList);
+//  void AddMissedTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<unsigned short>& tjl);
+  void TagShowerTjs(TjStuff& tjs, const CTP_t& inCTP, std::vector<std::vector<int>>& tjList);
+  void MergeTjList(std::vector<std::vector<int>>& tjList);
+  void MergeTjList2(TjStuff& tjs, std::vector<std::vector<int>>& tjList);
   void MergeOverlap(TjStuff& tjs, const CTP_t& inCTP, bool prt);
   void MergeSubShowers(TjStuff& tjs, const CTP_t& inCTP, bool prt);
   bool MergeShowersAndStore(TjStuff& tjs, unsigned short istj, unsigned short jstj, bool prt);
