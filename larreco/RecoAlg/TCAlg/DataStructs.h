d54df7d7518a005d6899585433d2ddc643c0b958
baller <baller@fnal.gov>
2017-08-02 13:26:45 -0500

    Eliminate TCHit X.

--- larreco/RecoAlg/TCAlg/DataStructs.h
+++ larreco/RecoAlg/TCAlg/DataStructs.h
@@ -159,7 +159,6 @@ namespace tca {
     raw::TDCtick_t StartTick {0};
     raw::TDCtick_t EndTick {0};
     float PeakTime {0};
-    float X;
     float SigmaPeakTime {1};
     float PeakAmplitude {1};
     float SigmaPeakAmp {1};
