05c507312fd5884dce78cc3a846af485d8152021
baller <baller@fnal.gov>
2017-08-22 10:18:16 -0500

    Added PFPStruct

--- larreco/RecoAlg/TCAlg/TCCR.h
+++ larreco/RecoAlg/TCAlg/TCCR.h
@@ -11,8 +11,8 @@
 #include "larreco/RecoAlg/TCAlg/DataStructs.h"
 
 namespace tca {
-  void SaveCRInfo(TjStuff& tjs, MatchStruct& ms, bool prt, bool fIsRealData);
-  int  GetOrigin(TjStuff& tjs, MatchStruct& ms);
+  void SaveCRInfo(TjStuff& tjs, PFPStruct& ms, bool prt, bool fIsRealData);
+  int  GetOrigin(TjStuff& tjs, PFPStruct& ms);
   void ClearCRInfo(TjStuff& tjs);
 }
 
