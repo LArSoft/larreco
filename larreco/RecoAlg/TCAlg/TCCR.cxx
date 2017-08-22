35f8504dc9d84f63d2603efca81ed9f3803dde1e
baller <baller@fnal.gov>
2017-08-22 10:18:27 -0500

    Added PFPStruct

--- larreco/RecoAlg/TCAlg/TCCR.cxx
+++ larreco/RecoAlg/TCAlg/TCCR.cxx
@@ -4,7 +4,7 @@
 namespace tca {
 
   ////////////////////////////////////////////////
-  void SaveCRInfo(TjStuff& tjs, MatchStruct& ms, bool prt, bool fIsRealData){
+  void SaveCRInfo(TjStuff& tjs, PFPStruct& pfp, bool prt, bool fIsRealData){
 
     //Check the origin of pfp
     if (tjs.SaveCRTree){
@@ -12,41 +12,41 @@ namespace tca {
         tjs.crt.cr_origin.push_back(-1);
       }
       else{
-        tjs.crt.cr_origin.push_back(GetOrigin(tjs, ms));
+        tjs.crt.cr_origin.push_back(GetOrigin(tjs, pfp));
       }
     }
 
     // save the xmin and xmax of each pfp
-    tjs.crt.cr_pfpxmin.push_back(std::min(ms.XYZ[0][0], ms.XYZ[1][0]));
-    tjs.crt.cr_pfpxmax.push_back(std::max(ms.XYZ[0][0], ms.XYZ[1][0]));
+    tjs.crt.cr_pfpxmin.push_back(std::min(pfp.XYZ[0][0], pfp.XYZ[1][0]));
+    tjs.crt.cr_pfpxmax.push_back(std::max(pfp.XYZ[0][0], pfp.XYZ[1][0]));
 
     //find max 
     const geo::TPCGeo &tpc = tjs.geom->TPC(0);
     float mindis0 = FLT_MAX;
     float mindis1 = FLT_MAX;
-    if (std::abs(ms.XYZ[0][1] - tpc.MinY())<mindis0) mindis0 = std::abs(ms.XYZ[0][1] - tpc.MinY());
-    if (std::abs(ms.XYZ[0][1] - tpc.MaxY())<mindis0) mindis0 = std::abs(ms.XYZ[0][1] - tpc.MaxY());
-    if (std::abs(ms.XYZ[0][2] - tpc.MinZ())<mindis0) mindis0 = std::abs(ms.XYZ[0][2] - tpc.MinZ());
-    if (std::abs(ms.XYZ[0][2] - tpc.MaxZ())<mindis0) mindis0 = std::abs(ms.XYZ[0][2] - tpc.MaxZ());
-    if (std::abs(ms.XYZ[1][1] - tpc.MinY())<mindis1) mindis1 = std::abs(ms.XYZ[1][1] - tpc.MinY());
-    if (std::abs(ms.XYZ[1][1] - tpc.MaxY())<mindis1) mindis1 = std::abs(ms.XYZ[1][1] - tpc.MaxY());
-    if (std::abs(ms.XYZ[1][2] - tpc.MinZ())<mindis1) mindis1 = std::abs(ms.XYZ[1][2] - tpc.MinZ());
-    if (std::abs(ms.XYZ[1][2] - tpc.MaxZ())<mindis1) mindis1 = std::abs(ms.XYZ[1][2] - tpc.MaxZ());
-    //std::cout<<ms.XYZ[0][1]<<" "<<ms.XYZ[0][2]<<" "<<ms.XYZ[1][1]<<" "<<ms.XYZ[1][2]<<" "<<tpc.MinY()<<" "<<tpc.MaxY()<<" "<<tpc.MinZ()<<" "<<tpc.MaxZ()<<" "<<mindis0<<" "<<mindis1<<" "<<mindis0+mindis1<<std::endl;
+    if (std::abs(pfp.XYZ[0][1] - tpc.MinY())<mindis0) mindis0 = std::abs(pfp.XYZ[0][1] - tpc.MinY());
+    if (std::abs(pfp.XYZ[0][1] - tpc.MaxY())<mindis0) mindis0 = std::abs(pfp.XYZ[0][1] - tpc.MaxY());
+    if (std::abs(pfp.XYZ[0][2] - tpc.MinZ())<mindis0) mindis0 = std::abs(pfp.XYZ[0][2] - tpc.MinZ());
+    if (std::abs(pfp.XYZ[0][2] - tpc.MaxZ())<mindis0) mindis0 = std::abs(pfp.XYZ[0][2] - tpc.MaxZ());
+    if (std::abs(pfp.XYZ[1][1] - tpc.MinY())<mindis1) mindis1 = std::abs(pfp.XYZ[1][1] - tpc.MinY());
+    if (std::abs(pfp.XYZ[1][1] - tpc.MaxY())<mindis1) mindis1 = std::abs(pfp.XYZ[1][1] - tpc.MaxY());
+    if (std::abs(pfp.XYZ[1][2] - tpc.MinZ())<mindis1) mindis1 = std::abs(pfp.XYZ[1][2] - tpc.MinZ());
+    if (std::abs(pfp.XYZ[1][2] - tpc.MaxZ())<mindis1) mindis1 = std::abs(pfp.XYZ[1][2] - tpc.MaxZ());
+    //std::cout<<pfp.XYZ[0][1]<<" "<<pfp.XYZ[0][2]<<" "<<pfp.XYZ[1][1]<<" "<<pfp.XYZ[1][2]<<" "<<tpc.MinY()<<" "<<tpc.MaxY()<<" "<<tpc.MinZ()<<" "<<tpc.MaxZ()<<" "<<mindis0<<" "<<mindis1<<" "<<mindis0+mindis1<<std::endl;
     tjs.crt.cr_pfpyzmindis.push_back(mindis0+mindis1);
 
     if (tjs.crt.cr_pfpxmin.back()<-2||
         tjs.crt.cr_pfpxmax.back()>260||
         tjs.crt.cr_pfpyzmindis.back()<30){
-      ms.CosmicScore = 1.;
+      pfp.CosmicScore = 1.;
     }
-    else ms.CosmicScore = 0;
+    else pfp.CosmicScore = 0;
 
     /*
     float minx = FLT_MAX;
     float maxx = FLT_MIN;
 
-    for(auto& tjID : ms.TjIDs) {
+    for(auto& tjID : pfp.TjIDs) {
       Trajectory& tj = tjs.allTraj[tjID - 1];
       TrajPoint& beginPoint = tj.Pts[tj.EndPt[0]];
       TrajPoint& endPoint = tj.Pts[tj.EndPt[1]];
@@ -67,19 +67,19 @@ namespace tca {
     tjs.crt.cr_pfpmintick.push_back(minx);
     tjs.crt.cr_pfpmaxtick.push_back(maxx);
     */
-//    std::cout<<ms.MCPartListIndex<<std::endl;
-//    std::cout<<ms.XYZ[0][0]<<" "<<ms.XYZ[1][0]<<std::endl;
+//    std::cout<<pfp.MCPartListIndex<<std::endl;
+//    std::cout<<pfp.XYZ[0][0]<<" "<<pfp.XYZ[1][0]<<std::endl;
 
   }
 
   ////////////////////////////////////////////////
-  int GetOrigin(TjStuff& tjs, MatchStruct& ms){
+  int GetOrigin(TjStuff& tjs, PFPStruct& pfp){
 
     art::ServiceHandle<cheat::BackTracker> bt;    
 
     std::map<int, float> omap; //<origin, energy>
 
-    for(auto& tjID : ms.TjIDs) {
+    for(auto& tjID : pfp.TjIDs) {
       
       Trajectory& tj = tjs.allTraj[tjID - 1];
       for(auto& tp : tj.Pts) {
