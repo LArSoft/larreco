12da20bc2e6621bd6503de9497d9bf9ea93c737c
Giuseppe Cerati <cerati@fnal.gov>
2017-08-29 11:49:57 -0500

    add TrackTrajectoryCreationBookKeeper

--- larreco/RecoAlg/TrackCreationBookKeeper.h
+++ larreco/RecoAlg/TrackCreationBookKeeper.h
@@ -8,9 +8,8 @@
 // Author: Giuseppe Cerati, cerati@fnal.gov
 ////////////////////////////////////////////////////////////////////////
 
+#include "larreco/RecoAlg/TrackTrajectoryCreationBookKeeper.h"
 #include "lardataobj/RecoBase/Track.h"
-#include "lardataobj/RecoBase/Hit.h"
-#include "lardataobj/RecoBase/TrackingTypes.h"
 #include "larreco/TrackFinder/TrackMaker.h"
 #include "lardataobj/RecoBase/TrackFitHitInfo.h"
 
@@ -32,9 +31,8 @@ namespace trkmkr {
   class TrackCreationBookKeeper {
   public:
   TrackCreationBookKeeper(std::vector<art::Ptr<Hit> >& outhits, OptionalOutputs& optionals, int tkID, int pdgHyp, bool hasMomenta, int nfitpars = 4)
-    : tkID_(tkID), pdgHyp_(pdgHyp), hasMomenta_(hasMomenta), totChi2_(0), hits(&outhits), opts(&optionals), nfittedpars(nfitpars)
+    : ttcbk_(outhits, hasMomenta), tkID_(tkID), pdgHyp_(pdgHyp), totChi2_(0), opts(&optionals), nfittedpars(nfitpars)
       {
-	hits->clear();
 	opts->reset();
       }
     //
@@ -45,24 +43,18 @@ namespace trkmkr {
     TrackCreationBookKeeper& operator=(TrackCreationBookKeeper&& ) = delete;
     //
     void addPoint(const Point_t& point, const Vector_t& vect, art::Ptr<Hit> hit, const PointFlags_t& flag, double chi2) {
-	positions.push_back(point);
-	momenta.push_back(vect);
-	hits->push_back(hit);
-	flags.push_back(flag);
-	chi2v.push_back(chi2);
-	totChi2_+=chi2;
+      ttcbk_.addPoint(point, vect, hit, flag);
+      chi2v.push_back(chi2);
+      totChi2_+=chi2;
     }
     void addPoint(const Point_t& point, const Vector_t& vect, art::Ptr<Hit> hit, const PointFlags_t& flag, double chi2, OptionalPointElement& ope) {
       addPoint(point, vect, hit, flag, chi2);
       opts->addPoint(ope);
     }
     void addPoint(Point_t&& point, Vector_t&& vect, art::Ptr<Hit> hit, PointFlags_t&& flag, double chi2) {
-	positions.push_back(std::move(point));
-	momenta.push_back(std::move(vect));
-	hits->push_back(hit);
-	flags.push_back(std::move(flag));
-	chi2v.push_back(chi2);
-	totChi2_+=chi2;
+      ttcbk_.addPoint(std::move(point), std::move(vect), hit, std::move(flag));
+      chi2v.push_back(chi2);
+      totChi2_+=chi2;
     }
     void addPoint(Point_t&& point, Vector_t&& vect, art::Ptr<Hit> hit, PointFlags_t&& flag, double chi2, OptionalPointElement& ope) {
       addPoint(std::move(point), std::move(vect), hit, std::move(flag), chi2);
@@ -72,25 +64,19 @@ namespace trkmkr {
     void setTotChi2(double totChi2) { totChi2_ = totChi2; }
     //
     Track finalizeTrack(const tracking::SMatrixSym55& covStart, const tracking::SMatrixSym55& covEnd) {
-      return Track(std::move(positions),std::move(momenta),std::move(flags),
-		   hasMomenta_,pdgHyp_,totChi2_,int(hits->size())-nfittedpars,
+      return Track(ttcbk_.finalizeTrackTrajectory(),pdgHyp_,totChi2_,int(chi2v.size())-nfittedpars,
 		   tracking::SMatrixSym55(covStart),tracking::SMatrixSym55(covEnd),tkID_);
     }
     Track finalizeTrack(tracking::SMatrixSym55&& covStart, tracking::SMatrixSym55&& covEnd) {
-      return Track(std::move(positions),std::move(momenta),std::move(flags),
-		   hasMomenta_,pdgHyp_,totChi2_,int(hits->size())-nfittedpars,
+      return Track(ttcbk_.finalizeTrackTrajectory(),pdgHyp_,totChi2_,int(chi2v.size())-nfittedpars,
 		   std::move(covStart),std::move(covEnd),tkID_);
     }
   private:
+    trkmkr::TrackTrajectoryCreationBookKeeper ttcbk_;
     int tkID_;
     int pdgHyp_;
-    bool hasMomenta_;
     double totChi2_;
-    std::vector<art::Ptr<Hit> >* hits;
     OptionalOutputs*             opts;
-    std::vector<Point_t>         positions;
-    std::vector<Vector_t>        momenta;
-    std::vector<PointFlags_t>    flags;
     std::vector<double>          chi2v;
     int nfittedpars; // hits are 1D measurement, i.e. each hit is one d.o.f.; no B field: 4 fitted parameters by default
     //
