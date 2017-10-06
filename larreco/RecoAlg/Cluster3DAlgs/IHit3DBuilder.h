b65bfe07d42cf702afef2678c93900135ff1b2e7
Usher, Tracy L <usher@slac.stanford.edu>
2017-10-06 07:26:29 -0700

    Building of 3D hits now done by art tools, add a tool which can take as input a 3D space point builder with associations to 2D hits (e.g. SpacePointSolver)

---
+++ larreco/RecoAlg/Cluster3DAlgs/IHit3DBuilder.h
@@ -0,0 +1,67 @@
+/**
+ *  @file   IHit3DBuilder.h
+ * 
+ *  @brief  This provides an art tool interface definition for tools which construct 3D hits used in 3D clustering
+ *
+ *  @author usher@slac.stanford.edu
+ * 
+ */
+#ifndef IHit3DBuilder_h
+#define IHit3DBuilder_h
+
+// Framework Includes
+#include "fhiclcpp/ParameterSet.h"
+#include "lardata/RecoObjects/Cluster3D.h"
+#include "art/Framework/Principal/Event.h"
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace lar_cluster3d
+{
+/**
+ *  @brief  IHit3DBuilder interface class definiton
+ */
+class IHit3DBuilder
+{
+public:
+    /**
+     *  @brief  Virtual Destructor
+     */
+    virtual ~IHit3DBuilder() noexcept = default;
+    
+    /**
+     *  @brief Interface for configuring the particular algorithm tool
+     *
+     *  @param ParameterSet  The input set of parameters for configuration
+     */
+    virtual void configure(const fhicl::ParameterSet&) = 0;
+    
+    /**
+     *  @brief Defines a structure mapping art representation to internal
+     */
+    using RecobHitToPtrMap = std::map<const recob::Hit*, art::Ptr<recob::Hit>>;
+
+    /**
+     *  @brief Given a set of recob hits, run DBscan to form 3D clusters
+     *
+     *  @param hitPairList           The input list of 3D hits to run clustering on
+     *  @param clusterParametersList A list of cluster objects (parameters from associated hits)
+     */
+    virtual void Hit3DBuilder(const art::Event&, reco::HitPairList&, RecobHitToPtrMap&) const = 0;
+
+    /**
+     *  @brief enumerate the possible values for time checking if monitoring timing
+     */
+    enum TimeValues {COLLECTARTHITS   = 0,
+                     BUILDTHREEDHITS  = 1,
+                     NUMTIMEVALUES
+    };
+    
+    /**
+     *  @brief If monitoring, recover the time to execute a particular function
+     */
+    virtual float getTimeToExecute(TimeValues index) const = 0;
+    
+};
+    
+} // namespace lar_cluster3d
+#endif
