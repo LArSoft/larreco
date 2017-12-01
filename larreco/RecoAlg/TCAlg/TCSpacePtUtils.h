6e560a3d7ae72688c43b8339b4f746f8956a1fe4
baller <baller@fnal.gov>
2017-12-01 10:11:41 -0600

    SpacePoint utilities

---
+++ larreco/RecoAlg/TCAlg/TCSpacePtUtils.h
@@ -0,0 +1,42 @@
+
+////////////////////////////////////////////////////////////////////////
+//
+//
+// TCAlg SpacePoint utilities
+//
+// Bruce Baller
+//
+///////////////////////////////////////////////////////////////////////
+#ifndef TRAJCLUSTERALGSPTUTILS_H
+#define TRAJCLUSTERALGSPTUTILS_H
+
+// C/C++ standard libraries
+#include <array>
+#include <vector>
+#include <bitset>
+#include <utility> // std::pair<>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// LArSoft libraries
+#include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
+#include "larreco/RecoAlg/TCAlg/DataStructs.h"
+#include "larreco/RecoAlg/TCAlg/DebugStruct.h"
+#include "larreco/RecoAlg/TCAlg/Utils.h"
+
+namespace tca {
+  
+  void Match3DSpts(TjStuff& tjs, const art::Event& evt, const geo::TPCID& tpcid, const art::InputTag& fSpacePointModuleLabel);
+  bool MergeBrokenTjs(TjStuff& tjs, std::vector<int>& tjInPln, bool prt);
+  std::vector<int> TjsNearSpacePts(TjStuff& tjs, std::vector<unsigned int> sptlist);
+  std::vector<unsigned int> SpacePtsAssociatedWith(TjStuff& tjs, const TrajPoint& tp);
+  std::vector<unsigned int> SpacePtsAssociatedWith(TjStuff& tjs, const Trajectory& tj);
+  std::vector<unsigned int> SpacePtsAtHit(TjStuff& tjs, unsigned int iht);
+  
+} // namespace tca
+
+#endif // ifndef TRAJCLUSTERALGSPTUTILS_H
