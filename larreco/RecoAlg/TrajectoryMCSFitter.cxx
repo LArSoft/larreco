85ca45c8ec58dcf98a8643eaa6287720dc90459c
Giuseppe Cerati <cerati@fnal.gov>
2017-11-30 18:01:07 -0600

    remove asserts

--- larreco/RecoAlg/TrajectoryMCSFitter.cxx
+++ larreco/RecoAlg/TrajectoryMCSFitter.cxx
@@ -30,7 +30,7 @@ recob::MCSFitResult TrajectoryMCSFitter::fitMcs(const recob::TrackTrajectory& tr
 	dtheta.push_back(-999.);
       } else { 
 	const double cosval = pcdir0.X()*pcdir1.X()+pcdir0.Y()*pcdir1.Y()+pcdir0.Z()*pcdir1.Z();
-	assert(std::abs(cosval)<=1);
+	//assert(std::abs(cosval)<=1);
 	//units are mrad
 	double dt = 1000.*acos(cosval);//should we try to use expansion for small angles?
 	dtheta.push_back(dt);
@@ -146,7 +146,7 @@ void TrajectoryMCSFitter::linearRegression(const recob::TrackTrajectory& traj, c
   const auto avgpos = middlePointCalc.middlePoint();
   const double norm = 1./double(npoints);
   //
-  assert(npoints>0);
+  //assert(npoints>0);
   //
   TMatrixDSym m(3);
   nextValid = firstPoint;
