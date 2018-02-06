d7b3eaf8a14c0af33c72da9c9d2de1e0932b5877
baller <baller@fnal.gov>
2018-02-06 13:19:07 -0600

    Work on Tp3 finding

--- larreco/RecoAlg/TCAlg/PFPUtils.h
+++ larreco/RecoAlg/TCAlg/PFPUtils.h
@@ -33,12 +33,15 @@ namespace tca {
   void UpdateMatchStructs(TjStuff& tjs, int oldTj, int newTj);
   void FillmAllTraj(TjStuff& tjs, const geo::TPCID& tpcid);
   void MakePFPTp3s(TjStuff& tjs, PFPStruct& pfp, bool anyTj);
+  void PickTp3s(TjStuff& tjs, PFPStruct& pfp, bool anyTj, bool prt);
+  void CheckXRange(TjStuff& tjs, PFPStruct& pfp, bool prt);
+  void AttachVertices(TjStuff& tjs, PFPStruct& pfpj, bool prt);
+  void FollowTp3s(TjStuff& tjs, PFPStruct& pfp, bool anyTj, bool prt);
   bool SetNewStart(TjStuff& tjs, PFPStruct& pfp, bool prt);
   void SetEndVx(TjStuff& tjs, PFPStruct& pfp, unsigned short atEnd, bool prt);
-  void SortByDistanceFromStart(TjStuff& tjs, PFPStruct& pfp);
-  bool CheckTp3Validity(TjStuff& tjs, PFPStruct& pfp, bool prt);
+  void SortByDistanceFromStart(TjStuff& tjs, PFPStruct& pfp, bool prt);
   void MoveTp3ToZ(TjStuff& tjs, TrajPoint3& tp3, double z);
-  void FitTp3(TjStuff& tjs, std::vector<TrajPoint3>& tp3s, unsigned short originPt, double fitLen, short fitDir, bool prt);
+  void FitTp3(TjStuff& tjs, std::vector<TrajPoint3>& tp3s, unsigned short originPt, double fitLen, short fitDir, bool originPtInFit, bool prt);
   void FindXMatches(TjStuff& tjs, unsigned short numPlanes, short maxScore, std::vector<MatchStruct>& matVec, bool prt);
   bool MakeTp3(TjStuff& tjs, const TrajPoint& itp, const TrajPoint& jtp, TrajPoint3& tp3);
   double DeltaAngle(const Vector3_t v1, const Vector3_t v2);
@@ -51,7 +54,6 @@ namespace tca {
   bool SplitAtKink(TjStuff& tjs, PFPStruct& pfp, double sep, bool prt);
   std::vector<unsigned short> FindKinks(const TjStuff& tjs, PFPStruct& pfp, double sep, bool prt);
   double KinkAngle(const TjStuff& tjs, const std::vector<TrajPoint3>& tp3s, unsigned short atPt, double sep);
-  void PrintTp3(std::string fcnLabel, const TjStuff& tjs, const TrajPoint3& tp3);
   PFPStruct CreatePFP(const TjStuff& tjs, const geo::TPCID& tpcid);
   void FindPFParticles(std::string fcnLabel, TjStuff& tjs, const geo::TPCID& tpcid, bool prt);
   bool DefinePFP(TjStuff& tjs, PFPStruct& pfp, bool prt);
@@ -59,8 +61,13 @@ namespace tca {
   void DefinePFPParents(TjStuff& tjs, const geo::TPCID& tpcid, bool prt);
   void DefinePFPParentsTestBeam(TjStuff& tjs, const geo::TPCID& tpcid, bool prt);
   bool StorePFP(TjStuff& tjs, PFPStruct& pfp);
-  void SetStopFlags(TjStuff& tjs, PFPStruct& pfp, bool prt);
+  void SetStopFlags(TjStuff& tjs, PFPStruct& pfp);
   bool InsideTPC(const TjStuff& tjs, Point3_t& pos, geo::TPCID& inTPCID);
+  void ReversePFP(TjStuff& tjs, PFPStruct& pfp);
+  void FixDirection(TjStuff& tjs, PFPStruct& pfp);
+  void FixDirection(Vector3_t& ofDir, const Vector3_t& usingDir);
+  void PrintTp3(std::string fcnLabel, const TjStuff& tjs, const TrajPoint3& tp3);
+  void PrintTp3s(std::string someText, const TjStuff& tjs, const PFPStruct& pfp, short printPts);
   
 } // namespace tca
 
