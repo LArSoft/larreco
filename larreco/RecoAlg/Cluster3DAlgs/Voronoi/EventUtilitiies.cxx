2e3cdfbc11cb3dd66e90a429c9720e82dbfcc296
Usher, Tracy L <usher@slac.stanford.edu>
2018-02-19 09:49:39 -0800

    Trying to optimize the utilities a bit, round off error is definitely an issue to keep aware of

--- larreco/RecoAlg/Cluster3DAlgs/Voronoi/EventUtilitiies.cxx
+++ larreco/RecoAlg/Cluster3DAlgs/Voronoi/EventUtilitiies.cxx
@@ -50,61 +50,35 @@ double EventUtilities::computeBreak(const double beachLinePos, const IEvent* lef
     double breakPoint = -std::numeric_limits<double>::max();
     
     // if the two are the same then the arcs are side-by-side and intersection is right in the middle
-    if (std::abs(deltaX1 - deltaX2) < 5.*std::numeric_limits<double>::epsilon()) breakPoint = 0.5 * (rightArc->yPos() + leftArc->yPos());
+    if (std::abs(deltaX1 - deltaX2) < std::numeric_limits<double>::epsilon()) breakPoint = 0.5 * (rightArc->yPos() + leftArc->yPos());
     
     // otherwise, we do the full calculation
     else
     {
         // set up for quadratic equation
-        double p1x = leftArc->xPos();
-        double p1y = leftArc->yPos();
-        double p2x = rightArc->xPos();
-        double p2y = rightArc->yPos();
-        double a   = p2x - p1x;
-        double b   = 2. * (p2y * deltaX1 - p1y * deltaX2);
-        double c   = deltaX2 * (p1y * p1y + deltaX1 * (p1x + lx)) - deltaX1 * (p2y * p2y + deltaX2 * (p2x + lx));
+        double p1x     = leftArc->xPos();
+        double p1y     = leftArc->yPos();
+        double p2x     = rightArc->xPos();
+        double p2y     = rightArc->yPos();
+        double a       = p2x - p1x;
+        double b       = 2. * (p2y * deltaX1 - p1y * deltaX2);
+        double c       = deltaX2 * (p1y * p1y + deltaX1 * (p1x + lx)) - deltaX1 * (p2y * p2y + deltaX2 * (p2x + lx));
+        double radical = std::max(0.,b * b - 4. * a * c);
         
-        double radical = b * (b - 4. * a * c / b);
+        if (radical > 0.) radical = sqrt(radical);
         
-        if (radical <= 0.)
-        {
-            std::cout << "***********************************************************" << std::endl;
-            std::cout << "This is a problem... radical: " << radical << ", a: " << a << ", b: " << b << ", c: " << c << ", eps: " << std::numeric_limits<double>::epsilon() << ", rat: " << radical / std::numeric_limits<double>::epsilon() << std::endl;
-            std::cout << "***********************************************************" << std::endl;
-            radical = 0.;
-        }
+        double rootPos = 0.5 * (-b + radical) / a;
+        double rootNeg = 0.5 * (-b - radical) / a;
         
-        radical = sqrt(radical);
-        
-        double rightRoot = std::max(0.5 * (-b + radical) / a, 0.5 * (-b - radical) / a);
-        double leftRoot  = std::min(0.5 * (-b + radical) / a, 0.5 * (-b - radical) / a);
-        
-        roots.first  = leftRoot;
-        roots.second = rightRoot;
+        roots.first  = std::min(rootPos, rootNeg);
+        roots.second = std::max(rootPos, rootNeg);
         
         // Ah yes, the eternal question... which solution?
         // Generally, we think we want the solution which is "between" the left and the right
         // However, it can be that the right arc is the remnant of an arc whose center lies to the
         // left of the center of the left arc, and vice versa.
-        // Generally, this breaks into two cases:
-        // a) The center of the left arc lies to the right of the center of the right arc
-        /*
-        if (p2y - p1y < std::numeric_limits<double>::epsilon())
-        {
-            // One more division, if the left arc is "higher" than the right then we want the leftmost solution
-            if (p1x > p2x) breakPoint = leftRoot;
-            else           breakPoint = rightRoot;
-        }
-        // b) the "standard" case where the left arc is left of the right arc and we want the solution between them
-        else
-        {
-            // Same as above, the root we want depends on which arc is higher
-            if (p1x > p2x) breakPoint = leftRoot;
-            else           breakPoint = rightRoot;
-        }
-        */
-        if (p1x > p2x) breakPoint = leftRoot;
-        else           breakPoint = rightRoot;
+        if (p1x < p2x) breakPoint = roots.second;
+        else           breakPoint = roots.first;
     }
     
     return breakPoint;
@@ -117,7 +91,7 @@ bool EventUtilities::newSiteToLeft(const IEvent* newSite, const IEvent* leftArc,
     // input arcs
     RootsPair roots;
     
-    double breakPoint = computeBreak(newSite->xPos(), leftArc, rightArc, roots);
+    double breakPoint = computeBreak(newSite->xPos()-0.000001, leftArc, rightArc, roots);
     
     return newSite->yPos() < breakPoint;
 }
