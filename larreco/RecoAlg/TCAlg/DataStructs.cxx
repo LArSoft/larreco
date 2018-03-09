c7dfa0619067e6a4cd13a156255dc9d65636433b
baller <baller@fnal.gov>
2018-03-09 15:22:11 -0600

    Comment out NeedsRebuild. 3D matching work.

--- larreco/RecoAlg/TCAlg/DataStructs.cxx
+++ larreco/RecoAlg/TCAlg/DataStructs.cxx
@@ -67,13 +67,6 @@ namespace tca {
     "SetDir"
   };
 
-  // bit names that describe the environment near a TP
-  const std::vector<std::string> TPEnvNames {
-    "EnvNearShower",
-    "EnvOverlap",
-    "EnvUnusedHits"
-  };
-  
   const std::vector<std::string> StopFlagNames {
     "Signal",
     "AtKink",
