f9981b279227d90d165addb7b5c431eac61083d1
baller <baller@fnal.gov>
2018-04-23 07:42:14 -0500

    Add PFPVxTjOK

--- larreco/RecoAlg/TCAlg/PFPUtils.h
+++ larreco/RecoAlg/TCAlg/PFPUtils.h
@@ -72,6 +72,7 @@ namespace tca {
   PFPStruct CreatePFP(const TjStuff& tjs, const geo::TPCID& tpcid);
   void FindPFParticles(std::string fcnLabel, TjStuff& tjs, const geo::TPCID& tpcid, bool prt);
   bool DefinePFP(std::string inFcnLabel, TjStuff& tjs, PFPStruct& pfp, bool prt);
+  bool PFPVxTjOK(TjStuff& tjs, PFPStruct& pfp, bool prt);
   void PFPVertexCheck(TjStuff& tjs);
   void AnalyzePFP(TjStuff& tjs, PFPStruct& pfp, bool prt);
   void DefinePFPParents(TjStuff& tjs, const geo::TPCID& tpcid, bool prt);
