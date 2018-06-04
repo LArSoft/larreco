67b32fe2e178a6c8f556c40c2e1b211845163885
baller <baller@fnal.gov>
2018-06-04 11:00:26 -0500

    remove obsolete algs

--- larreco/RecoAlg/TCAlg/DataStructs.cxx
+++ larreco/RecoAlg/TCAlg/DataStructs.cxx
@@ -57,8 +57,6 @@ namespace tca {
     "KillInShowerVx",
     "ShowerTj",
     "ShwrParent",
-    "ChkShwrParEnd",  // Ensure that the end of a shower parent already inside a shower has an end near a shower end
-    "KillShwrNuPFP",  // Kill neutrino PFP particles with a vertex inside a shower
     "MergeOverlap",
     "MergeSubShowers",
     "MergeSubShowersTj",
