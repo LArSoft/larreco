87237e4d8acf63f7cc8f283a442ece5999135dd8
baller <baller@fnal.gov>
2018-06-04 11:01:10 -0500

    remove obsolete algs. change unsigned shorts to ints. Add DontClusterStruct

--- larreco/RecoAlg/TCAlg/DataStructs.h
+++ larreco/RecoAlg/TCAlg/DataStructs.h
@@ -116,7 +116,7 @@ namespace tca {
     float Score {0};
     short Wire {-1};                 // wire number for an incomplete 3D vertex
     geo::TPCID TPCID;
-    std::array<unsigned short, 3> Vx2ID {{0}}; // List of 2D vertex IDs in each plane
+    std::array<int, 3> Vx2ID {{0}}; // List of 2D vertex IDs in each plane
     int ID {0};          // 0 = obsolete vertex
     bool Primary {false};
     bool Neutrino {false};
@@ -284,8 +284,8 @@ namespace tca {
     float ParentFOM {10};
     int ID {0}; 
     int ParentID {0};  // The ID of a parent Tj - the one at the start of the shower
-    unsigned short TruParentID {0};
-    unsigned short SS3ID {0};     // ID of a ShowerStruct3D to which this 2D shower is matched
+    int TruParentID {0};
+    int SS3ID {0};     // ID of a ShowerStruct3D to which this 2D shower is matched
     bool NeedsUpdate {true};       // Needs to be updated (e.g. after adding a tj, defining a parent, etc)
   };
   
@@ -315,6 +315,12 @@ namespace tca {
     unsigned short PFPIndex {USHRT_MAX};    // The index of the pfp for this shower
     int Vx3ID {0};
     bool NeedsUpdate {true};       // This is set true whenever the shower needs to be updated
+  };
+  
+  struct DontClusterStruct {
+    std::array<int, 2> TjIDs;     // pairs of Tjs that shouldn't be clustered in shower reconstruction because...
+    int Vx2ID;                    // they share a 2D vertex that may be matched to...
+    int Vx3ID;                    // a high-score 3D vertex
   };
 
   struct ShowerTreeVars {
@@ -417,8 +423,6 @@ namespace tca {
     kKillInShowerVx,
     kShowerTj,
     kShwrParent,
-    kChkShwrParEnd,
-    kKillShwrNuPFP,
     kMergeOverlap,
     kMergeSubShowers,
     kMergeSubShowersTj,
@@ -495,7 +499,7 @@ namespace tca {
     std::vector<MatchStruct> matchVec; ///< 3D matching vector
     std::vector<PFPStruct> pfps;
     std::vector<ShowerStruct> cots;       // Clusters of Trajectories that define 2D showers
-    std::vector<std::array<int, 2>> dontCluster; // pairs of Tjs that shouldn't clustered in one shower
+    std::vector<DontClusterStruct> dontCluster; // pairs of Tjs that shouldn't clustered in one shower
     std::vector<ShowerStruct3D> showers;  // 3D showers
     std::vector<float> Vertex2DCuts; ///< Max position pull, max Position error rms
     std::vector<float> Vertex3DCuts;   ///< 2D vtx -> 3D vtx matching cuts 
