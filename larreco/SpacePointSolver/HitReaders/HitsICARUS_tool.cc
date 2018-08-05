dd3a10e9a3a102f8476f56a88046f34f72da1ad2
Usher, Tracy L <usher@slac.stanford.edu>
2018-08-05 06:52:36 -0700

    Implement a simple tool interface to cleanly read hits from a specific detector into the solver

---
+++ larreco/SpacePointSolver/HitReaders/HitsICARUS_tool.cc
@@ -0,0 +1,110 @@
+/**
+ *  @file   HitsICARUS_tool.cc
+ *
+ *  @brief  art Tool for comparing clusters and merging those that are consistent
+ *
+ */
+
+// Framework Includes
+#include "art/Utilities/ToolMacros.h"
+#include "art/Utilities/make_tool.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "cetlib/search_path.h"
+#include "cetlib/cpu_timer.h"
+
+#include "larreco/SpacePointSolver/HitReaders/IHitReader.h"
+
+// LArSoft includes
+#include "lardata/Utilities/AssociationUtil.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/PlaneGeo.h"
+#include "larcorealg/Geometry/WireGeo.h"
+
+// std includes
+#include <string>
+#include <functional>
+#include <iostream>
+#include <memory>
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// implementation follows
+
+namespace reco3d {
+    
+class HitsICARUS : virtual public IHitReader
+{
+public:
+    /**
+     *  @brief  Constructor
+     *
+     *  @param  pset
+     */
+    explicit HitsICARUS(const fhicl::ParameterSet&);
+    
+    /**
+     *  @brief  Destructor
+     */
+    ~HitsICARUS();
+    
+    void configure(fhicl::ParameterSet const &pset) override;
+    
+    /**
+     *  @brief Scan an input collection of clusters and modify those according
+     *         to the specific implementing algorithm
+     *
+     *  @param clusterParametersList A list of cluster objects (parameters from associated hits)
+     */
+    bool readHits(const std::vector<art::Ptr<recob::Hit>>&,            // input hits
+                  std::vector<art::Ptr<recob::Hit>>&,                  // output hits plane 0
+                  std::vector<art::Ptr<recob::Hit>>&,                  // output hits plane 1
+                  std::vector<art::Ptr<recob::Hit>>&) const override;  // output hits plane 2
+};
+
+HitsICARUS::HitsICARUS(fhicl::ParameterSet const &pset)
+{
+    this->configure(pset);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+HitsICARUS::~HitsICARUS()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void HitsICARUS::configure(fhicl::ParameterSet const &pset)
+{
+    //    m_enableMonitoring   = pset.get<bool>  ("EnableMonitoring",  true  );
+    
+    return;
+}
+
+bool HitsICARUS::readHits(const std::vector<art::Ptr<recob::Hit>>& inputHits,           // input hits
+                          std::vector<art::Ptr<recob::Hit>>&       collectionHits,      // output hits plane 0
+                          std::vector<art::Ptr<recob::Hit>>&       firstIndHits,        // output hits plane 1
+                          std::vector<art::Ptr<recob::Hit>>&       secondIndHits) const // output hits plane 2
+{
+    for(auto& hit: inputHits)
+    {
+        if(isnan(hit->Integral()) || isinf(hit->Integral()))
+        {
+            mf::LogWarning("Hits_ICARUS") << "WARNING: bad recob::Hit::Integral() = "
+            << hit->Integral()
+            << ". Skipping." << std::endl;
+            continue;
+        }
+        
+        if      (hit->WireID().Plane == 0) firstIndHits.push_back(hit);
+        else if (hit->WireID().Plane == 1) secondIndHits.push_back(hit);
+        else                               collectionHits.push_back(hit);
+    } // end for hit
+    
+    mf::LogDebug("Hits_ICARUS") << ">>>>> Reading hits done" << std::endl;
+    
+    return false;
+}
+
+DEFINE_ART_CLASS_TOOL(HitsICARUS)
+} // namespace lar_cluster3d
---
+++ larreco/SpacePointSolver/HitReaders/IHitReader.h
@@ -0,0 +1,54 @@
+/**
+ *  @file   IHitReader.h
+ * 
+ *  @brief  This provides an art tool interface definition for reading hits into the SpacePointSolver universe
+ *
+ *  @author usher@slac.stanford.edu
+ * 
+ */
+#ifndef IHitReader_h
+#define IHitReader_h
+
+// Framework Includes
+#include "fhiclcpp/ParameterSet.h"
+
+// Algorithm includes
+#include "canvas/Persistency/Common/Ptr.h"
+#include "lardataobj/RecoBase/Hit.h"
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+namespace reco3d
+{
+/**
+ *  @brief  IHitReader interface class definiton
+ */
+class IHitReader
+{
+public:
+    /**
+     *  @brief  Virtual Destructor
+     */
+    virtual ~IHitReader() noexcept = default;
+    
+    /**
+     *  @brief Interface for configuring the particular algorithm tool
+     *
+     *  @param ParameterSet  The input set of parameters for configuration
+     */
+    virtual void configure(const fhicl::ParameterSet&) = 0;
+ 
+    /**
+     *  @brief Scan an input collection of clusters and modify those according
+     *         to the specific implementing algorithm
+     *
+     *  @param clusterParametersList A list of cluster objects (parameters from associated hits)
+     */
+    virtual bool readHits(const std::vector<art::Ptr<recob::Hit>>&,       // input hits
+                          std::vector<art::Ptr<recob::Hit>>&,             // output hits plane 0
+                          std::vector<art::Ptr<recob::Hit>>&,             // output hits plane 1
+                          std::vector<art::Ptr<recob::Hit>>&) const = 0;  // output hits plane 2
+    
+};
+    
+} // namespace lar_cluster3d
+#endif
