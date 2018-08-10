351179815d4599299484a5d27fd55c84a4cb33b7
Alex Himmel <ahimmel@fnal.gov>
2018-08-10 11:51:18 -0500

    Introduce a new service interface, IPhotonCalibrator, for calibrating optical hits (ADC -> PE). Includes a simple PhotonCalibratorStandard which applies a common linear conversion which does not vary by channel (e.g. sensible behavior for simulation).

---
+++ larreco/Calibrator/PhotonCalibratorStandard.h
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////////////////
+// \file PhotonCalibratorStandard.h
+//
+// \brief Standard service provider applying a flat scale factor to all optical hits.
+//
+// \author ahimmel@fnal.gov
+//
+////////////////////////////////////////////////////////////////////////
+
+
+#ifndef PHOTONCALIBRATORDEFAULT_H
+#define PHOTONCALIBRATORDEFAULT_H
+
+#include "larreco/Calibrator/IPhotonCalibrator.h"
+
+// ART includes
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+
+
+namespace calib {
+
+  class PhotonCalibratorStandard : public IPhotonCalibrator
+  {
+  public:
+    PhotonCalibratorStandard(fhicl::ParameterSet const& pset, art::ActivityRegistry& reg)
+      : fSPESize  ( pset.get< float >("SPESize")     ), 
+        fSPEShift ( pset.get< float >("SPEShift", 0.)),
+        fUseArea  ( pset.get< bool  >("UseArea")     )
+      {}
+
+    
+    PhotonCalibratorStandard(float size, float shift, bool useArea)
+      : fSPESize  ( size ),
+        fSPEShift ( shift ),
+        fUseArea  ( useArea )
+      {}      
+
+    // Override base class functions
+    double PE(double adcs, int opchannel) const override { return adcs/fSPESize + fSPEShift; }
+    bool   UseArea() const override                      { return fUseArea; }
+
+    // Setters for this implementation
+    void SetSPESize (float size)    { fSPESize  = size; }
+    void SetSPEShift(float shift)   { fSPEShift = shift; }
+    void SetUseArea (bool  useArea) { fUseArea  = useArea; }
+        
+    /// Need a 3D position because result depends on position along length of
+    /// bar. This is going to be pretty imprecise even so.
+    // virtual double GeV(double PE, int opchannel, TVector3 pos) override;
+
+  private:
+    float  fSPESize;
+    float  fSPEShift;
+    bool   fUseArea;
+
+    
+  }; // class PhotonCalibratorStandard
+}
+
+DECLARE_ART_SERVICE_INTERFACE_IMPL(calib::PhotonCalibratorStandard, calib::IPhotonCalibrator, LEGACY)
+
+
+#endif
