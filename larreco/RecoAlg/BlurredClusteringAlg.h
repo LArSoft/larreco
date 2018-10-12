8357001d6de7717c351e0918b4695a773af8d61f
Kyle Knoepfel <knoepfel@fnal.gov>
2018-10-11 21:05:45 -0500

    Fix bug #21041: Illegal memory accessing in BlurredClusterAlg.
    This commit includes converting from DOS to UNIX format.

--- larreco/RecoAlg/BlurredClusteringAlg.h
+++ larreco/RecoAlg/BlurredClusteringAlg.h
@@ -13,13 +13,11 @@
 #define BlurredClustering_h
 
 // Framework includes
-#include "canvas/Persistency/Common/FindManyP.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Optional/TFileService.h"
 #include "art/Framework/Services/Optional/TFileDirectory.h"
-#include "messagefacility/MessageLogger/MessageLogger.h"
 
 // LArSoft includes
 #include "larcore/CoreUtils/ServiceUtil.h" // lar::providerFrom<>()
@@ -70,70 +68,72 @@ class cluster::BlurredClusteringAlg {
 public:
 
   BlurredClusteringAlg(fhicl::ParameterSet const& pset);
-  virtual ~BlurredClusteringAlg();
-
-  void reconfigure(fhicl::ParameterSet const&p);
+  ~BlurredClusteringAlg();
 
   /// Create the PDF to save debug images
   void CreateDebugPDF(int run, int subrun, int event);
 
   /// Takes a vector of clusters (itself a vector of hits) and turns them into clusters using the initial hit selection
-  void ConvertBinsToClusters(std::vector<std::vector<double> > const& image,
-			     std::vector<std::vector<int> > const& allClusterBins,
-			     std::vector<art::PtrVector<recob::Hit> >& clusters);
+  void ConvertBinsToClusters(std::vector<std::vector<double>> const& image,
+                             std::vector<std::vector<unsigned int>> const& allClusterBins,
+                             std::vector<art::PtrVector<recob::Hit>>& clusters) const;
 
   /// Takes hit map and returns a 2D vector representing wire and tick, filled with the charge
-  std::vector<std::vector<double> > ConvertRecobHitsToVector(std::vector<art::Ptr<recob::Hit> > const& hits);
+  std::vector<std::vector<double>> ConvertRecobHitsToVector(std::vector<art::Ptr<recob::Hit>> const& hits);
 
   /// Find clusters in the histogram
-  int FindClusters(std::vector<std::vector<double> > const& image, std::vector<std::vector<int> >& allcluster);
+  unsigned int FindClusters(std::vector<std::vector<double>> const& image,
+                            std::vector<std::vector<unsigned int>>& allcluster);
 
   /// Find the global wire position
   int GlobalWire(geo::WireID const& wireID);
 
   /// Applies Gaussian blur to image
-  std::vector<std::vector<double> > GaussianBlur(std::vector<std::vector<double> > const& image);
+  std::vector<std::vector<double>> GaussianBlur(std::vector<std::vector<double>> const& image);
 
   /// Minimum size of cluster to save
-  unsigned int GetMinSize() { return fMinSize; }
+  unsigned int GetMinSize() const noexcept { return fMinSize; }
 
   /// Converts a 2D vector in a histogram for the debug pdf
-  TH2F* MakeHistogram(std::vector<std::vector<double> > const& image, TString name);
+  TH2F* MakeHistogram(std::vector<std::vector<double>> const& image, TString name);
 
   /// Save the images for debugging
   /// This version takes the final clusters and overlays on the hit map
-  void SaveImage(TH2F* image, std::vector<art::PtrVector<recob::Hit> > const& allClusters, int pad, int tpc, int plane);
+  void SaveImage(TH2F* image, std::vector<art::PtrVector<recob::Hit>> const& allClusters, int pad, int tpc, int plane);
 
   /// Save the images for debugging
   void SaveImage(TH2F* image, int pad, int tpc, int plane);
 
   /// Save the images for debugging
   /// This version takes a vector of bins and overlays the relevant bins on the hit map
-  void SaveImage(TH2F* image, std::vector<std::vector<int> > const& allClusterBins, int pad, int tpc, int plane);
+  void SaveImage(TH2F* image, std::vector<std::vector<unsigned int>> const& allClusterBins, int pad, int tpc, int plane);
 
 private:
 
   /// Converts a vector of bins into a hit selection - not all the hits in the bins vector are real hits
-  art::PtrVector<recob::Hit> ConvertBinsToRecobHits(std::vector<std::vector<double> > const& image, std::vector<int> const& bins);
+  art::PtrVector<recob::Hit> ConvertBinsToRecobHits(std::vector<std::vector<double>> const& image,
+                                                    std::vector<unsigned int> const& bins) const;
 
   /// Converts a bin into a recob::Hit (not all of these bins correspond to recob::Hits - some are fake hits created by the blurring)
-  art::Ptr<recob::Hit> ConvertBinToRecobHit(std::vector<std::vector<double> > const& image, int bin);
+  art::Ptr<recob::Hit> ConvertBinToRecobHit(std::vector<std::vector<double>> const& image, unsigned int bin) const;
 
-  /// Converts an xbin and a ybin to a global bin number                                                                                                                       
-  int ConvertWireTickToBin(std::vector<std::vector<double> > const& image, int xbin, int ybin);
+  /// Converts an xbin and a ybin to a global bin number
+  unsigned int ConvertWireTickToBin(std::vector<std::vector<double>> const& image,
+                                    unsigned int xbin,
+                                    unsigned int ybin) const;
 
-  /// Returns the charge stored in the global bin value                                                                                                                        
-  double ConvertBinToCharge(std::vector<std::vector<double> > const& image, int bin);
+  /// Returns the charge stored in the global bin value
+  double ConvertBinToCharge(std::vector<std::vector<double>> const& image, unsigned int bin) const;
 
   /// Count how many dead wires there are in the blurring region for a particular hit
   /// Returns a pair of counters representing how many dead wires there are below and above the hit respectively
-  std::pair<int,int> DeadWireCount(int wire_bin, int width);
+  std::pair<unsigned int, unsigned int> DeadWireCount(unsigned int wire_bin, unsigned int width) const;
 
   /// Dynamically find the blurring radii and Gaussian sigma in each dimension
-  void FindBlurringParameters(int& blurwire, int& blurtick, int& sigmawire, int& sigmatick);
+  std::array<unsigned int, 4> FindBlurringParameters();
 
   /// Returns the hit time of a hit in a particular bin
-  double GetTimeOfBin(std::vector<std::vector<double> > const& image, int bin);
+  double GetTimeOfBin(std::vector<std::vector<double>> const& image, int bin);
 
   /// Makes all the kernels which could be required given the tuned parameters
   void MakeKernels();
@@ -148,35 +148,34 @@ private:
   std::string fDetector;
 
   // Parameters used in the Blurred Clustering algorithm
-  int          fBlurWire;                 // blur radius for Gauss kernel in the wire direction
-  int          fBlurTick;                 // blur radius for Gauss kernel in the tick direction
+  unsigned int fBlurWire;                 // blur radius for Gauss kernel in the wire direction
+  unsigned int fBlurTick;                 // blur radius for Gauss kernel in the tick direction
   double       fSigmaWire;                // sigma for Gaussian kernel in the wire direction
   double       fSigmaTick;                // sigma for Gaussian kernel in the tick direction
-  int          fMaxTickWidthBlur;         // maximum distance to blur a hit based on its natural width in time
-  int          fClusterWireDistance;      // how far to cluster from seed in wire direction
-  int          fClusterTickDistance;      // how far to cluster from seed in tick direction
-//  unsigned int fMinMergeClusterSize;      // minimum size of a cluster to consider merging it to another
+  unsigned int fMaxTickWidthBlur;         // maximum distance to blur a hit based on its natural width in time
+  unsigned int fClusterWireDistance;      // how far to cluster from seed in wire direction
+  unsigned int fClusterTickDistance;      // how far to cluster from seed in tick direction
   unsigned int fNeighboursThreshold;      // min. number of neighbors to add to cluster
-  int          fMinNeighbours;            // minumum number of neighbors to keep in the cluster
+  unsigned int fMinNeighbours;            // minumum number of neighbors to keep in the cluster
   unsigned int fMinSize;                  // minimum size for cluster
   double       fMinSeed;                  // minimum seed after blurring needed before clustering proceeds
   double       fTimeThreshold;            // time threshold for clustering
   double       fChargeThreshold;          // charge threshold for clustering
 
   // Blurring stuff
-  int fKernelWidth, fKernelHeight;
-  std::vector<std::vector<std::vector<double> > > fAllKernels;
+  unsigned int fKernelWidth, fKernelHeight;
+  std::vector<std::vector<std::vector<double>>> fAllKernels;
 
   // Hit containers
-  std::vector<std::vector<art::Ptr<recob::Hit> > > fHitMap;
+  std::vector<std::vector<art::Ptr<recob::Hit>>> fHitMap;
   std::vector<bool> fDeadWires;
 
-  int fLowerTick, fUpperTick;
-  int fLowerWire, fUpperWire;
+  unsigned int fLowerTick, fUpperTick;
+  unsigned int fLowerWire, fUpperWire;
 
   // For the debug pdf
-  TCanvas* fDebugCanvas;
-  std::string fDebugPDFName;
+  TCanvas* fDebugCanvas{nullptr};
+  std::string fDebugPDFName{};
 
   // art service handles
   art::ServiceHandle<geo::Geometry> fGeom;
