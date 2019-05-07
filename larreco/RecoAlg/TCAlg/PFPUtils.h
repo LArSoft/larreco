05d1cf3b94d66972f1308756d88c5f06d525a39b
baller <baller@fnal.gov>
2019-05-07 11:18:56 -0400

    Ongoing 3D re-write

--- larreco/RecoAlg/TCAlg/PFPUtils.h
+++ larreco/RecoAlg/TCAlg/PFPUtils.h
@@ -35,7 +35,6 @@
 namespace tca {
 
   void StitchPFPs();
-//  void UpdateMatchStructs(TCSlice& slc, int oldTj, int newTj);
   void FindPFParticles(TCSlice& slc);
   void ChkPFPMC(TCSlice& slc, PFPStruct& pfp);
   void ReconcileTPs(TCSlice& slc);
