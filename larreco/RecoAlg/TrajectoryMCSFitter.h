daede45f3c3057f4b1bf486b6c25c4f1e6785afd
Giuseppe Cerati <cerati@fnal.gov>
2019-06-03 15:56:10 -0500

    update to TrajectoryMCSFitter for MicroBooNE MCC9.1

--- larreco/RecoAlg/TrajectoryMCSFitter.h
+++ larreco/RecoAlg/TrajectoryMCSFitter.h
@@ -3,6 +3,7 @@
 
 #include "fhiclcpp/ParameterSet.h"
 #include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Sequence.h"
 #include "fhiclcpp/types/Table.h"
 #include "canvas/Persistency/Common/Ptr.h"
 #include "lardataobj/RecoBase/MCSFitResult.h"
@@ -22,7 +23,9 @@ namespace trkf {
    *
    * Outputs are: a recob::MCSFitResult, containing:
    *   resulting momentum, momentum uncertainty, and best likelihood value (both for fwd and bwd fit);
-   *   vector of segment (radiation) lengths, vector of scattering angles, and PID hypothesis used in the fit.
+   *   vector of comulative segment (radiation) lengths, vector of scattering angles, and PID hypothesis used in the fit.
+   *   Note that the comulative segment length is what is used to compute the energy loss, but the segment length is actually slightly different,
+   *   so the output can be used to reproduce the original results but they will not be identical (but very close).
    *
    * For configuration options see TrajectoryMCSFitter#Config
    *
@@ -77,9 +80,19 @@ namespace trkf {
 	Comment("Maximum momentum value in likelihood scan."),
 	7.50  
       };
+      fhicl::Atom<double> pStepCoarse {
+        Name("pStepCoarse"),
+	Comment("Step in momentum value in initial coase likelihood scan."),
+	0.01
+      };
       fhicl::Atom<double> pStep {
         Name("pStep"),
-	Comment("Step in momentum value in likelihood scan."),
+	Comment("Step in momentum value in fine grained likelihood scan."),
+	0.01
+      };
+      fhicl::Atom<double> fineScanWindow {
+        Name("fineScanWindow"),
+	Comment("Window size for fine grained likelihood scan around result of coarse scan."),
 	0.01
       };
       fhicl::Atom<double> angResol {
@@ -87,10 +100,25 @@ namespace trkf {
 	Comment("Angular resolution parameter used in modified Highland formula. Unit is mrad."),
 	3.0
       };
+      fhicl::Sequence<double, 5> hlParams {
+        Name("hlParams"),
+	Comment("Parameters for tuning of Highland formula. Default is pdg value of 13.6. For values as in https://arxiv.org/abs/1703.0618 set to [0.1049,0.,11.0038,0,0]. Formula is hlParams_[0]/(p*p) + hlParams_[1]/p + hlParams_[2] + hlParams_[3]*p + hlParams_[4]*p*p."),
+	  {0.,0.,13.6,0,0}
+      };
+      fhicl::Atom<double> segLenTolerance {
+        Name("segLenTolerance"),
+	Comment("Tolerance in actual segment length (lower bound)."),
+	1.0
+      };
+      fhicl::Atom<bool> applySCEcorr {
+        Name("applySCEcorr"),
+	Comment("Flag to turn the Space Charge Effect correction on/off."),
+	false
+      };
     };
     using Parameters = fhicl::Table<Config>;
     //
-    TrajectoryMCSFitter(int pIdHyp, int minNSegs, double segLen, int minHitsPerSegment, int nElossSteps, int eLossMode, double pMin, double pMax, double pStep, double angResol){
+    TrajectoryMCSFitter(int pIdHyp, int minNSegs, double segLen, int minHitsPerSegment, int nElossSteps, int eLossMode, double pMin, double pMax, double pStepCoarse, double pStep, double fineScanWindow, double angResol, const std::array<double, 5>& hlParams, double segLenTolerance, bool applySCEcorr){
       pIdHyp_ = pIdHyp;
       minNSegs_ = minNSegs;
       segLen_ = segLen;
@@ -99,11 +127,16 @@ namespace trkf {
       eLossMode_ = eLossMode;
       pMin_ = pMin;
       pMax_ = pMax;
+      pStepCoarse_ = pStepCoarse;
       pStep_ = pStep;
+      fineScanWindow_ = fineScanWindow;
       angResol_ = angResol;
+      hlParams_ = hlParams;
+      segLenTolerance_ = segLenTolerance;
+      applySCEcorr_ = applySCEcorr;
     }
     explicit TrajectoryMCSFitter(const Parameters & p)
-      : TrajectoryMCSFitter(p().pIdHypothesis(),p().minNumSegments(),p().segmentLength(),p().minHitsPerSegment(),p().nElossSteps(),p().eLossMode(),p().pMin(),p().pMax(),p().pStep(),p().angResol()) {}
+      : TrajectoryMCSFitter(p().pIdHypothesis(),p().minNumSegments(),p().segmentLength(),p().minHitsPerSegment(),p().nElossSteps(),p().eLossMode(),p().pMin(),p().pMax(),p().pStepCoarse(),p().pStep(),p().fineScanWindow(),p().angResol(),p().hlParams(),p().segLenTolerance(),p().applySCEcorr()) {}
     //
     recob::MCSFitResult fitMcs(const recob::TrackTrajectory& traj, bool momDepConst = true) const { return fitMcs(traj,pIdHyp_,momDepConst); }
     recob::MCSFitResult fitMcs(const recob::Track& track,          bool momDepConst = true) const { return fitMcs(track,pIdHyp_,momDepConst); }
@@ -128,12 +161,11 @@ namespace trkf {
     };
     //
     const ScanResult doLikelihoodScan(std::vector<float>& dtheta, std::vector<float>& seg_nradlengths, std::vector<float>& cumLen, bool fwdFit, bool momDepConst, int pid) const;
+    const ScanResult doLikelihoodScan(std::vector<float>& dtheta, std::vector<float>& seg_nradlengths, std::vector<float>& cumLen, bool fwdFit, bool momDepConst, int pid,
+				      float pmin, float pmax, float pstep) const;
     //
     inline double MomentumDependentConstant(const double p) const {
-      //these are from https://arxiv.org/abs/1703.06187
-      constexpr double a = 0.1049;
-      constexpr double c = 11.0038;
-      return (a/(p*p)) + c;
+      return hlParams_[0]/(p*p) + hlParams_[1]/p + hlParams_[2] + hlParams_[3]*p + hlParams_[4]*p*p;
     }
     double mass(int pid) const {
       if (abs(pid)==13)   { return mumass; }
@@ -156,8 +188,13 @@ namespace trkf {
     int    eLossMode_;
     double pMin_;
     double pMax_;
+    double pStepCoarse_;
     double pStep_;
+    double fineScanWindow_;
     double angResol_;
+    std::array<double, 5> hlParams_;
+    double segLenTolerance_;
+    bool   applySCEcorr_;
   };
 }
 
