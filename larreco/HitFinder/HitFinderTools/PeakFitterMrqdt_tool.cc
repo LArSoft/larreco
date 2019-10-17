10d6a7cf69091a1a4f8171e7bd7b643ca6a2162d
Giuseppe Cerati <cerati@fnal.gov>
2019-10-17 15:03:32 -0500

    small fixes, plus add boundaries to fit parameters

--- larreco/HitFinder/HitFinderTools/PeakFitterMrqdt_tool.cc
+++ larreco/HitFinder/HitFinderTools/PeakFitterMrqdt_tool.cc
@@ -41,7 +41,6 @@ namespace reco_tool
     double fPeakRange;
     double fAmpRange;
     bool fFloatBaseline;
-    //bool fOutputHistograms; // unused
 
     std::unique_ptr<gshf::MarqFitAlg> fMarqFitAlg;
 
@@ -79,7 +78,6 @@ namespace reco_tool
 					   double&                                     chi2PerNDF, 
 					   int&                                        NDF) const
   {
-
     if(fhc_vec.empty()) return;
 
     const float chiCut   = 1e-3;
@@ -92,41 +90,48 @@ namespace reco_tool
 
     int roiSize = endTime - startTime;
 
+    // init to a large number in case the fit fails
+    chi2PerNDF = chiSqr;
+
     std::vector<float> y(roiSize);
     std::vector<float> p(3*fhc_vec.size());
+    std::vector<float> plimmin(3*fhc_vec.size());
+    std::vector<float> plimmax(3*fhc_vec.size());
     std::vector<float> perr(3*fhc_vec.size());
 
     /* choose the fit function and set the parameters */
     nParams = 0;
 
     for(size_t ih=0; ih<fhc_vec.size(); ih++){
-      float const peakMean   = fhc_vec[ih].hitCenter - (float)startTime;
+      float const peakMean   = fhc_vec[ih].hitCenter - 0.5 - (float)startTime; //shift by 0.5 to account for bin width
       float const peakWidth  = fhc_vec[ih].hitSigma;
       float const amplitude  = fhc_vec[ih].hitHeight;
-      //float const meanLowLim = fmax(peakMean - fPeakRange * peakWidth,       0.);
-      //float const meanHiLim  = fmin(peakMean + fPeakRange * peakWidth, (float)roiSize);
+      float const meanLowLim = fmax(peakMean - fPeakRange * peakWidth,       0.);
+      float const meanHiLim  = fmin(peakMean + fPeakRange * peakWidth, (float)roiSize);
       p[0+nParams]=amplitude;
       p[1+nParams]=peakMean;
       p[2+nParams]=peakWidth;
+
+      plimmin[0+nParams]=amplitude*0.1;
+      plimmin[1+nParams]=meanLowLim;
+      plimmin[2+nParams]=std::max(fMinWidth, 0.1 * peakWidth);
+
+      plimmax[0+nParams]=amplitude*fAmpRange;
+      plimmax[1+nParams]=meanHiLim;
+      plimmax[2+nParams]=fMaxWidthMult * peakWidth;
 
       nParams += 3;
     }
     int fitResult=-1;
 
-    //recast signal -> y to keep the  catch about negative adc values
     for(size_t idx=0; idx<size_t(roiSize); idx++){
-      // float adc=signal[startTime+idx];
-      //if(adc<=0.) adc=0;
-      //y[idx]=adc;
-
-      if(signal[startTime+idx]<=0.) y[idx]=0;
-      else y[idx]=signal[startTime+idx];
+      y[idx]=signal[startTime+idx];
     }
 
     int trial=0;
     lambda=-1.;   /* initialize lambda on first call */
     do{
-      fitResult=fMarqFitAlg->gshf::MarqFitAlg::mrqdtfit(lambda, &p[0], &y[0], nParams, roiSize, chiSqr, dchiSqr);
+      fitResult=fMarqFitAlg->gshf::MarqFitAlg::mrqdtfit(lambda, &p[0], &plimmin[0], &plimmax[0], &y[0], nParams, roiSize, chiSqr, dchiSqr);
       trial++;
       if(fitResult||(trial>100))break;
     }
@@ -152,9 +157,9 @@ namespace reco_tool
 	  PeakFitParams_t mhpp;	  
 	  mhpp.peakAmplitude      = p[parIdx + 0];
           mhpp.peakAmplitudeError = perr[parIdx + 0];
-          mhpp.peakCenter         = p[parIdx + 1] + 0.5 + float(startTime);
+          mhpp.peakCenter         = p[parIdx + 1] + 0.5 + float(startTime); //shift by 0.5 to account for bin width
           mhpp.peakCenterError    = perr[parIdx + 1];
-          mhpp.peakSigma          = p[parIdx + 2];
+          mhpp.peakSigma          = std::abs(p[parIdx + 2]);
           mhpp.peakSigmaError     = perr[parIdx + 2];
   
 	  mhpp_vec.emplace_back(mhpp);
