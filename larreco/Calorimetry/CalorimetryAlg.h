95a2adc0a4a288ed2ddd3f8be2ff05bbaa0591a9
Edward <e.tyley@sheffield.ac.uk>
2020-04-03 11:59:10 -0500

    Overloaded CalorimetryAlg to allow for local E field recombination factor when calculating dEdx

--- larreco/Calorimetry/CalorimetryAlg.h
+++ larreco/Calorimetry/CalorimetryAlg.h
@@ -20,117 +20,171 @@
 #include <vector>
 
 namespace detinfo {
-  class DetectorClocksData;
-  class DetectorPropertiesData;
+class DetectorClocksData;
+class DetectorPropertiesData;
 }
 
 namespace recob {
-  class Hit;
+class Hit;
 }
 
 /// General LArSoft Utilities
 namespace calo {
-  class CalorimetryAlg {
+class CalorimetryAlg {
   public:
-    struct Config {
-      using Name = fhicl::Name;
-      using Comment = fhicl::Comment;
+  struct Config {
+    using Name = fhicl::Name;
+    using Comment = fhicl::Comment;
 
-      fhicl::Sequence<double> CalAmpConstants{
-        Name("CalAmpConstants"),
-        Comment("ADC to electrons constants for each plane.")};
-
-      fhicl::Sequence<double> CalAreaConstants{
-        Name("CalAreaConstants"),
-        Comment("Area to electrons constants for each plane.")};
-
-      fhicl::Atom<bool> CaloUseModBox{Name("CaloUseModBox"),
-                                      Comment("Use modified box model if true, birks otherwise")};
-
-      fhicl::Atom<int> CaloLifeTimeForm{Name("CaloLifeTimeForm"),
-                                        Comment("0 = exponential, 1 = exponential + constant")};
-
-      fhicl::Atom<bool> CaloDoLifeTimeCorrection{Name("CaloDoLifeTimeCorrection"),
-                                                 Comment("Apply lifetime correction if true")};
+    fhicl::Sequence<double> CalAmpConstants {
+      Name("CalAmpConstants"),
+      Comment("ADC to electrons constants for each plane.")
     };
 
-    CalorimetryAlg(const fhicl::ParameterSet& pset)
+    fhicl::Sequence<double> CalAreaConstants {
+      Name("CalAreaConstants"),
+      Comment("Area to electrons constants for each plane.")
+    };
+
+    fhicl::Atom<bool> CaloUseModBox { Name("CaloUseModBox"),
+      Comment("Use modified box model if true, birks otherwise") };
+
+    fhicl::Atom<int> CaloLifeTimeForm { Name("CaloLifeTimeForm"),
+      Comment("0 = exponential, 1 = exponential + constant") };
+
+    fhicl::Atom<bool> CaloDoLifeTimeCorrection { Name("CaloDoLifeTimeCorrection"),
+      Comment("Apply lifetime correction if true") };
+  };
+
+  CalorimetryAlg(const fhicl::ParameterSet& pset)
       : CalorimetryAlg(fhicl::Table<Config>(pset, {})())
-    {}
+  {
+  }
 
-    CalorimetryAlg(const Config& config);
+  CalorimetryAlg(const Config& config);
 
-    double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
-                    detinfo::DetectorPropertiesData const& det_prop,
-                    recob::Hit const& hit,
-                    double pitch,
-                    double T0 = 0) const;
-    double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
-                    detinfo::DetectorPropertiesData const& det_prop,
-                    double dQ,
-                    double time,
-                    double pitch,
-                    unsigned int plane,
-                    double T0 = 0) const;
-    double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
-                    detinfo::DetectorPropertiesData const& det_prop,
-                    double dQdx,
-                    double time,
-                    unsigned int plane,
-                    double T0 = 0) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      recob::Hit const& hit,
+      double pitch,
+      double T0 = 0) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQ,
+      double time,
+      double pitch,
+      unsigned int plane,
+      double T0 = 0) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx,
+      double time,
+      unsigned int plane,
+      double T0 = 0) const;
 
-    // FIXME: How may of these are actually used?
-    double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
-                     detinfo::DetectorPropertiesData const& det_prop,
-                     recob::Hit const& hit,
-                     double pitch,
-                     double T0 = 0) const;
-    double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
-                     detinfo::DetectorPropertiesData const& det_prop,
-                     double dQ,
-                     double time,
-                     double pitch,
-                     unsigned int plane,
-                     double T0 = 0) const;
-    double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
-                     detinfo::DetectorPropertiesData const& det_prop,
-                     double dQdx,
-                     double time,
-                     unsigned int plane,
-                     double T0 = 0) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      recob::Hit const& hit,
+      double pitch,
+      double T0,
+      double EField) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQ,
+      double time,
+      double pitch,
+      unsigned int plane,
+      double T0,
+      double EField) const;
+  double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx,
+      double time,
+      unsigned int plane,
+      double T0,
+      double EField) const;
 
-    double
-    ElectronsFromADCPeak(double adc, unsigned short plane) const
-    {
-      return adc / fCalAmpConstants[plane];
-    }
+  // FIXME: How may of these are actually used?
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      recob::Hit const& hit,
+      double pitch,
+      double T0 = 0) const;
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQ,
+      double time,
+      double pitch,
+      unsigned int plane,
+      double T0 = 0) const;
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx,
+      double time,
+      unsigned int plane,
+      double T0 = 0) const;
 
-    double
-    ElectronsFromADCArea(double area, unsigned short plane) const
-    {
-      return area / fCalAreaConstants[plane];
-    }
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      recob::Hit const& hit,
+      double pitch,
+      double T0,
+      double EField) const;
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQ,
+      double time,
+      double pitch,
+      unsigned int plane,
+      double T0,
+      double EField) const;
+  double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx,
+      double time,
+      unsigned int plane,
+      double T0,
+      double EField) const;
 
-    double LifetimeCorrection(detinfo::DetectorClocksData const& clock_data,
-                              detinfo::DetectorPropertiesData const& det_prop,
-                              double time,
-                              double T0 = 0) const;
+  double
+  ElectronsFromADCPeak(double adc, unsigned short plane) const
+  {
+    return adc / fCalAmpConstants[plane];
+  }
+
+  double
+  ElectronsFromADCArea(double area, unsigned short plane) const
+  {
+    return area / fCalAreaConstants[plane];
+  }
+
+  double LifetimeCorrection(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double time,
+      double T0 = 0) const;
 
   private:
-    art::ServiceHandle<geo::Geometry const> geom;
+  art::ServiceHandle<geo::Geometry const> geom;
 
-    double dEdx_from_dQdx_e(detinfo::DetectorClocksData const& clock_data,
-                            detinfo::DetectorPropertiesData const& det_prop,
-                            double dQdx_e,
-                            double time,
-                            double T0 = 0) const;
+  double dEdx_from_dQdx_e(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx_e,
+      double time,
+      double T0 = 0) const;
 
-    std::vector<double> const fCalAmpConstants;
-    std::vector<double> const fCalAreaConstants;
-    bool const fUseModBox;
-    int const fLifeTimeForm;
-    bool const fDoLifeTimeCorrection;
+  double dEdx_from_dQdx_e(detinfo::DetectorClocksData const& clock_data,
+      detinfo::DetectorPropertiesData const& det_prop,
+      double dQdx_e,
+      double time,
+      double T0,
+      double EField) const;
 
-  }; // class CalorimetryAlg
+  std::vector<double> const fCalAmpConstants;
+  std::vector<double> const fCalAreaConstants;
+  bool const fUseModBox;
+  int const fLifeTimeForm;
+  bool const fDoLifeTimeCorrection;
+
+}; // class CalorimetryAlg
 } // namespace calo
 #endif // UTIL_CALORIMETRYALG_H
