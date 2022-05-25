f51032f6c7e83b9e81f4b0e767c8416db78acbd2
Wenjie Wu <whuwenjie@gmail.com>
2022-05-25 14:46:28 -0500

    solution for pandoraTrack to deal with invalid points

--- larreco/RecoAlg/TrackMomentumCalculator.cxx
+++ larreco/RecoAlg/TrackMomentumCalculator.cxx
@@ -500,6 +500,7 @@ namespace trkf {
     int n_points = trk->NumberTrajectoryPoints();
 
     for (int i = 0; i < n_points; i++) {
+      if (checkValidPoints && !trk->HasValidPoint(i)) continue;
       auto const& pos = trk->LocationAtPoint(i);
       recoX.push_back(pos.X());
       recoY.push_back(pos.Y());
