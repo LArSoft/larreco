fc790c0f80d4fc90d892abe467be4084835e2d6e
gputnam <gputnam@icarusbuild02.fnal.gov>
2023-07-28 15:43:52 -0500

    Allow to configure recombination parameters. Allow for angular dependence in parameters coupled to E-field.

--- larreco/Calorimetry/CalorimetryAlg.h
+++ larreco/Calorimetry/CalorimetryAlg.h
@@ -12,10 +12,14 @@
 #include "fhiclcpp/types/Atom.h"
 #include "fhiclcpp/types/Sequence.h"
 #include "fhiclcpp/types/Table.h"
+#include "fhiclcpp/types/OptionalSequence.h"
 
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "larcore/Geometry/Geometry.h"
+#include "larcoreobj/SimpleTypesAndConstants/PhysicalConstants.h"
 #include "lardataobj/RecoBase/Hit.h"
+
+#include "TF1.h"
 
 #include <vector>
 
@@ -52,6 +56,42 @@ namespace calo {
 
       fhicl::Atom<bool> CaloDoLifeTimeCorrection{Name("CaloDoLifeTimeCorrection"),
                                                  Comment("Apply lifetime correction if true")};
+
+      fhicl::Atom<double> ModBoxA {
+        Name("ModBoxA"),
+        Comment("Alpha value in modified box recombination."),
+        util::kModBoxA
+      };
+
+      fhicl::Atom<std::string> ModBoxBTF1 {
+        Name("ModBoxBTF1"),
+        Comment("String compiled into a TF1. Should return the Mod-Box beta value as a function of phi."),
+        "[0]"
+      };
+
+      fhicl::OptionalSequence<double> ModBoxBParam {
+        Name("ModBoxBParam"),
+        Comment("Parameters for the ModBoxBTF1 function.")
+      };
+
+      fhicl::Atom<double> BirksA {
+        Name("BirksA"),
+        Comment("Alpha value in modified box recombination."),
+        util::kRecombA
+      };
+
+      fhicl::Atom<std::string> BirksKTF1 {
+        Name("BirksKTF1"),
+        Comment("String compiled into a TF1. Should return the Birks k value as a function of phi."),
+        "[0]"
+      };
+
+      fhicl::OptionalSequence<double> BirksKParam {
+        Name("BirksKParam"),
+        Comment("Parameters for the BirksKTF1 function. List of doubles.")
+      };
+
+
     };
 
     CalorimetryAlg(const fhicl::ParameterSet& pset)
@@ -67,39 +107,25 @@ namespace calo {
                     double T0 = 0) const;
     double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
                     detinfo::DetectorPropertiesData const& det_prop,
-                    double dQ,
-                    double time,
-                    double pitch,
-                    unsigned int plane,
-                    double T0 = 0) const;
-    double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
-                    detinfo::DetectorPropertiesData const& det_prop,
                     double dQdx,
                     double time,
                     unsigned int plane,
                     double T0 = 0) const;
-
     double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
                     detinfo::DetectorPropertiesData const& det_prop,
                     recob::Hit const& hit,
                     double pitch,
                     double T0,
-                    double EField) const;
-    double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
-                    detinfo::DetectorPropertiesData const& det_prop,
-                    double dQ,
-                    double time,
-                    double pitch,
-                    unsigned int plane,
-                    double T0,
-                    double EField) const;
+                    double EField,
+                    double phi=90) const;
     double dEdx_AMP(detinfo::DetectorClocksData const& clock_data,
                     detinfo::DetectorPropertiesData const& det_prop,
                     double dQdx,
                     double time,
                     unsigned int plane,
                     double T0,
-                    double EField) const;
+                    double EField,
+                    double phi=90) const;
 
     // FIXME: How may of these are actually used?
     double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
@@ -109,39 +135,25 @@ namespace calo {
                      double T0 = 0) const;
     double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
                      detinfo::DetectorPropertiesData const& det_prop,
-                     double dQ,
-                     double time,
-                     double pitch,
-                     unsigned int plane,
-                     double T0 = 0) const;
-    double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
-                     detinfo::DetectorPropertiesData const& det_prop,
                      double dQdx,
                      double time,
                      unsigned int plane,
                      double T0 = 0) const;
-
     double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
                      detinfo::DetectorPropertiesData const& det_prop,
                      recob::Hit const& hit,
                      double pitch,
                      double T0,
-                     double EField) const;
-    double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
-                     detinfo::DetectorPropertiesData const& det_prop,
-                     double dQ,
-                     double time,
-                     double pitch,
-                     unsigned int plane,
-                     double T0,
-                     double EField) const;
+                     double EField,
+                     double phi=90) const;
     double dEdx_AREA(detinfo::DetectorClocksData const& clock_data,
                      detinfo::DetectorPropertiesData const& det_prop,
                      double dQdx,
                      double time,
                      unsigned int plane,
                      double T0,
-                     double EField) const;
+                     double EField,
+                     double phi=90) const;
 
     double ElectronsFromADCPeak(double adc, unsigned short plane) const
     {
@@ -158,6 +170,10 @@ namespace calo {
                               double time,
                               double T0 = 0) const;
 
+    // Recombination corrections
+    double BirksCorrection(double dQdx, double phi, double rho, double E_field) const;
+    double ModBoxCorrection(double dQdx, double phi, double rho, double E_field) const;
+
   private:
     art::ServiceHandle<geo::Geometry const> geom;
 
@@ -171,13 +187,20 @@ namespace calo {
                             double dQdx_e,
                             double time,
                             double T0,
-                            double EField) const;
+                            double EField,
+                            double phi=90) const;
 
     std::vector<double> const fCalAmpConstants;
     std::vector<double> const fCalAreaConstants;
     bool const fUseModBox;
     int const fLifeTimeForm;
     bool const fDoLifeTimeCorrection;
+
+    // Recombination parameters
+    double fModBoxA; // Mod-Box alpha
+    TF1 fModBoxBF; // Function of phi to get the Mod-Box beta value
+    double fBirksA; // Birks A cosntant
+    TF1 fBirksKF; // Function of phi to get the Birks-k value
 
   }; // class CalorimetryAlg
 } // namespace calo
