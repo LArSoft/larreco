d0a7b3053906781426357ae89829148e1bc1c720
Thomas Junk <trj@fnal.gov>
2024-10-24 15:47:17 -0500

    put in a check on the iterator for the hit sum accumulation -- end cannot be after begin

--- larreco/HitFinder/GausHitFinder_module.cc
+++ larreco/HitFinder/GausHitFinder_module.cc
@@ -535,6 +535,7 @@ namespace hit {
 
                 //protection to avoid negative ranges
                 if (newright - newleft < 0) continue;
+		if (HitsumStartItr > HitsumEndItr) continue;
 
                 //avoid ranges out of ROI if it happens
                 if (HitsumStartItr < sumStartItr) HitsumStartItr = sumStartItr;
